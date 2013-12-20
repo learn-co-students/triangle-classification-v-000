a6f807b8e655b1ed361b42465f0708eb366df842
Spencer Rogers <spencer1248@gmail.com>
2013-12-20 15:05:54 -0500

    Repackage

---
+++ README.md
@@ -0,0 +1,15 @@
+# Triangle
+
+Write code to make the tests in `triangle_spec.rb` pass.
+
+## Hint
+
+The sum of the lengths of any two sides of a triangle always exceeds the length of the third side, a principle known as the _triangle inequality_.
+
+## Building and Raising a Custom Error
+
+Read: 
+* http://ruby.about.com/od/advancedruby/qt/quickexceptions.htm
+* http://www.skorks.com/2009/09/ruby-exceptions-and-exception-handling/
+* http://phrogz.net/programmingruby/tut_exceptions.html
+* http://ablogaboutcode.com/2011/01/03/using-custom-error-messages-for-cleaner-code/
--- triangle_spec.rb
+++ triangle_spec.rb
@@ -1,21 +1,3 @@
-# # Triangle
-
-# Write a program that can tell you if a triangle is equilateral, isosceles, or scalene.
-
-# As a bonus, the program should raise an error if the triangle cannot exist.
-
-# ## Hint
-
-# The sum of the lengths of any two sides of a triangle always exceeds the length of the third side, a principle known as the _triangle inequality_.
-
-## Building and Raising a Custom Error
-
-# Read: 
-# http://ruby.about.com/od/advancedruby/qt/quickexceptions.htm
-# http://www.skorks.com/2009/09/ruby-exceptions-and-exception-handling/
-# http://phrogz.net/programmingruby/tut_exceptions.html
-# http://ablogaboutcode.com/2011/01/03/using-custom-error-messages-for-cleaner-code/
-
 RSpec.configure do |config|
   # Use color in STDOUT
   config.color_enabled = true
