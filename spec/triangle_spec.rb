249db96b9778cfd5cb87425b330185ce6d38b491
Logan Hasson <logan.hasson@gmail.com>
2014-01-16 12:12:17 -0500

    Add boilerplate file and organize dir structure

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,13 +1,4 @@
-RSpec.configure do |config|
-  # Use color in STDOUT
-  config.color_enabled = true
-
-  # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
-
-  # Use the specified formatter
-  config.formatter = :progress # :progress, :html, :textmate
-end
+require 'spec_helper'
 
 describe 'Triangle' do
 
