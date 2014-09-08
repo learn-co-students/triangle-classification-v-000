15df73de338fafed56ab348b6a8a914951c55d02
Katie Hoffman <ktahoffman@gmail.com>
2014-09-08 13:25:42 -0400

    add passing code

--- triangle.rb
+++ triangle.rb
@@ -1,3 +1,45 @@
+# Write your code here.
+
+class TriangleError < StandardError
+end
+
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    sides = [a,b,c]
+    ineq = [a+b > c, a+c > b, b+c > a]
+
+    if sides.min <= 0 || ineq.include?(false)
+      raise TriangleError
+    end
+
+    if equilateral?
+      :equilateral
+    elsif scalene?
+      :scalene
+    elsif isosceles?
+      :isosceles
+    end
+  end
+
+  def equilateral?
+    a == b && b == c
+  end
+
+  def scalene?
+    a != b && a != c && b != c
+  end
+
+  def isosceles?
+    (a == b && b != c) || (b == c && c != a) || (a == c && c != b)
+  end
+  
 end
