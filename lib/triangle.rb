fe46e8f60e8c9da08d272f71f72c70918f662397
Frank Nowinski <nowinski.frank@gmail.com>
2015-11-17 22:08:33 -0500

    Update triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,16 +7,14 @@ class Triangle
 
   def valid_triangle?
     valid = true 
-
     [one, two, three].each { |x| valid = false if x <= 0 }
     valid = false if ((one+two) <= three) || ((one+three) <= two) || ((two+three) <= one)
-
     valid
   end
 
   def kind 
     raise TriangleError if !valid_triangle?
-
+    
     if (one == two) && (one == three)
       :equilateral
     elsif (one == two) || (one == three) || (two == three)
