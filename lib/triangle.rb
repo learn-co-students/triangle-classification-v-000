18d97b5fed9c4d6076452614cd2c8c8990798c9a
onyoo <roberto9m9@gmail.com>
2015-11-18 22:45:23 +0100

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1,l2,l3)
+    @l1=l1
+    @l2=l2
+    @l3=l3
+  end
+
+  def kind
+    if [@l1,@l2,@l3].any?{|o| o <= 0} || [@l1,@l2,@l3].any?{|o| (@l1+@l2+@l3-o) <= o}
+      raise TriangleError
+    elsif (@l1 == @l2) && (@l2 == @l3)
+      :equilateral
+    elsif (@l1==@l2) || (@l2==@l3) || (@l3==@l1)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
