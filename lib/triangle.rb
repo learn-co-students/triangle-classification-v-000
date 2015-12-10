77e5fe7128b13a0cbb1f27a6e8cff29a77e88801
lisamarie616 <lisamarie616@gmail.com>
2015-12-09 16:57:28 -0800

    refactor method to validate triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,26 +8,33 @@ class Triangle
     @c = c
   end
 
-  def no_size
-    self.a <= 0 || self.b <= 0 || self.c <= 0
-  end
+  # def no_size
+  #   a <= 0 || b <= 0 || c <= 0
+  # end
 
-  def triangle_inequality
-    self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
+  # def triangle_inequality
+  #   a + b <= c || a + c <= b || b + c <= a
+  # end
+
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a,b,c].each {|s| real_triangle << false if s <= 0}
+    raise TriangleError if real_triangle.include?(false)    
   end
 
   def kind
-    if no_size || triangle_inequality
-      raise TriangleError
+    # if no_size || triangle_inequality
+    #   raise TriangleError
+    # else
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || a == c || b == c
+      :isosceles
     else
-      if a == b && b == c
-        :equilateral
-      elsif a == b || a == c || b == c
-        :isosceles
-      else
-        :scalene
-      end
+      :scalene
     end
+    # end
   end
 
 end
