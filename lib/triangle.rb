8b35f6b6f04a848f4166c736ee23a11629c7c924
lisamarie616 <lisamarie616@gmail.com>
2015-12-09 16:49:13 -0800

    define a custom error and use it

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_reader :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def no_size
+    self.a <= 0 || self.b <= 0 || self.c <= 0
+  end
+
+  def triangle_inequality
+    self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
+  end
+
+  def kind
+    if no_size || triangle_inequality
+      raise TriangleError
+    else
+      if a == b && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
