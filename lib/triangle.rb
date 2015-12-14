e5d314d5a20b136a55f05afda7bc89f5464c9a20
authorbeard <msnolan@gmail.com>
2015-12-14 11:44:38 -0500

    all tests passing

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :kind
+  attr_reader :equilateral, :isosceles, :scalene
+
+
+  def initialize(side_1, side_2, side_3)
+    @sides = [side_1, side_2, side_3]
+    self.legal?
+    if @sides.uniq.count == 1
+      @kind = :equilateral
+    elsif @sides.uniq.count == 2
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end  
+  end
+
+  def legal?
+    @sum = @sides.reduce(:+).to_f
+    if @sides.any?{|side| side <= 0} || @sides.any?{|side| (@sum-side) <= side}
+      raise TriangleError
+    else
+      return true
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "This is not a triangle. Each side must be greater than 0, and the sum of any two sides must be greater than the third side."  
+  end
 end
