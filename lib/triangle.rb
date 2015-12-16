49967038b37b9c6e895707f9aac20fa6ee8b70fb
yoshikot <ytamaoki@gmail.com>
2015-12-16 17:08:23 -0500

    done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    array = [a, b, c].sort
+    @a = array[0]
+    @b = array[1]
+    @c = array[2]
+  end
+
+  def kind
+    if @a <= 0 || @a + @b <= @c
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral 
+    elsif @a == @b && @b != @c || @a != @b && @b == @c
+      :isosceles
+    else :scalene
+    end
+  end  
+end
+
+class TriangleError < StandardError
 end
