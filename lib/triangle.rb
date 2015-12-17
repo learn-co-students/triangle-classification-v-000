735aaf5cfc0359f1f2f9b6fcb354e3ae006f4d1e
Alvinluipo <lualvin1234@gmail.com>
2015-12-16 21:11:34 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind
+
+    if (a+b<=c || b+c<=a || a+c<=b || a<=0 || b<=0 || c<=0)
+      raise TriangleError
+          puts error.message
+    else
+      if a==b && b==c
+        :equilateral
+      elsif a==b || a==c ||b==c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+end
+  
+
+class TriangleError < StandardError
+  def message
+    puts "The sum of any two sides of a triangle must exceed the value of the third side. Also, all three sides must be greater than 0."
+  end
 end
