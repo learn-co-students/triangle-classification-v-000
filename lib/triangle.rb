28d95dd14359b5ba0c4103d31d597816d71f0f48
jsgoldb <jgoldb@yahoo.com>
2015-12-30 15:41:48 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if invalid? == true
+      raise TriangleError
+    end
+    if side1 == side2 && side2 == side3
+      return :equilateral
+    elsif side1 != side2 && side2 != side3 && side1 != side3 
+      return :scalene
+    else
+      return :isosceles
+    end
+      
+  end
+
+  def invalid?
+    side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1 || side1 == 0 || side2 == 0 || side3 == 0
+  end
+end
+
+class TriangleError < StandardError
+
 end
