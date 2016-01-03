92665053beef1bb374bc36a71465f2c53c9cb6d9
zacscodingclub <zbaston@gmail.com>
2016-01-02 18:48:08 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,47 @@
 class Triangle
-  # write code here
+  attr_reader :side_a, :side_b, :side_c, :sides
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+
+    import
+  end
+
+  def import
+    @sides = [@side_a, @side_b, @side_c]
+  end
+
+  def kind
+    if invalid? || check_zero
+      throw_error
+    elsif @side_a == @side_b && @side_a == @side_c
+      :equilateral
+    elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def check_zero
+    @sides.any? { |side| side == 0 }
+  end
+
+  def invalid?
+    @side_a + @side_b <= @side_c || @side_a + @side_c <= @side_b || @side_b + @side_c <= @side_a ? true : false
+  end
+
+  def throw_error
+    begin
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Those dimensions don't make a triangle. Please try again."
+  end
 end
