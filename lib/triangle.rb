3964b96c9c0530d141e9f252826d8b6ebc626513
fiveinfinity <jlachance1@gmail.com>
2016-01-25 13:12:11 -0600

    added 'kind' method to check if triangle was legal, and returned what type it is

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    side = [a,b,c]
+
+    side.sort!.each_index do |i|
+      if (side[i] <= 0) || (side[i] + (side[i+1])) <= side[i+2]
+        raise TriangleError
+      elsif side[i] == side[i+1] && side[i] == side[i+2]
+        return :equilateral
+      elsif side[i] == side[i+1] || side[i] == side[i+2] || side[i+1] == side[i+2]
+        return :isosceles
+      elsif side[i] != side[i+1] && side[i] != side[i+2] && side[i+1] != side[i+2]
+        return :scalene
+      end
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+  def error_code
+      "You're throwing this fancy Triangle Error!"
+  end
 end
