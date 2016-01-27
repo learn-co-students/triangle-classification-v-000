c93daa06760ce828d11259513dc64ad74f45a879
jenjiyi <jenny.yamada@gmail.com>
2016-01-27 10:46:39 -0800

    done with lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+  def initialize (s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    if s1 + s2 <= s3 || s2 + s3 <= s1 || s3 + s1 <= s2 
+       raise TriangleError
+    elsif s1 <= 0 || s2 <= 0 || s3 <= 0
+       raise TriangleError
+    elsif s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s3 == s1
+      :isosceles
+    elsif s1 != s2 && s2 != s3
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+ def message
+    "This is not a valid triangle"
+  end
 end
