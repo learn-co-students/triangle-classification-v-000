b7a6e99a8b40f8606d2424bf23d3acb3933e1d1b
krisnotchris <krishenderson@gmail.com>
2016-02-03 19:58:15 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1 <= 0 || side2 <= 0 || side3 <= 0 || side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+        raise TriangleError
+    elsif side1 == side2 && side2 == side3
+      return :equilateral
+    elsif side1 == side2 || side3 == side1 || side2 == side3
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+
+  
+end
+
+class TriangleError < StandardError
+  def message
+    "A triangle cannot have a side equal to 0!"
+  end
 end
