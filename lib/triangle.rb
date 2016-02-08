0aeb550600a6475dec6e2de740adb06378463070
zachnewburgh <zach.newburgh@gmail.com>
2016-02-08 09:01:27 -0500

    Triangle Error and #kind

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
   # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    if side_one <= 0 || side_two <= 0 || side_three <= 0
+      raise TriangleError
+    elsif (side_one + side_two) <= side_three || (side_one + side_three) <= side_two || (side_two + side_three) <= side_one
+      raise TriangleError
+    elsif side_one == side_two && side_one == side_three
+      :equilateral
+    elsif side_one == side_two || side_one == side_three || side_two == side_three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
