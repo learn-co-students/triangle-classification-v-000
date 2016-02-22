3fb8137264965df046a2205d9ac9c232c1f3e4d1
rocketNeck <homestarflyer@gmail.com>
2016-02-22 14:34:48 -0500

    all done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  def sides
+    @sides.sort!
+  end
+
+  def initialize(a, b, c)
+    @sides = [a, b, c]
+  end
+
+  def badtriangle?
+    sides.any? == 0 || sides[0] + sides[1] <= sides[2]
+  end
+
+  def kind
+    if badtriangle?
+      raise TriangleError
+    else
+      if sides[0] == sides[2]
+        :equilateral
+      elsif sides[0] == sides[1] || sides[1] == sides[2]
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
+  puts "gimmi a real triangle"
 end
