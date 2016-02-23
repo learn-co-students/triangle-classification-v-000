9892690fd707aa3bff8f90c972d3dfed194b3c5f
Brian Holland <brian.holland@proatria.com>
2016-02-23 10:06:25 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def valid_triangle?
+    if (@a<=0 || @b<=0 || @c<=0)
+      raise TriangleError
+    elsif (@a+@b<=@c || @a+@c<=@b || @b+@c<=@a)
+      raise TriangleError
+    end
+    return true
+  end
+
+  def kind
+    !valid_triangle?
+    if (@a==@b && @a==@c && @b==@c)
+      return :equilateral
+    elsif (@a==@b || @b==@c || @a==@c)
+      return :isosceles
+    else
+      return :scalene
+    end
+    raise TriangleError
+  end
+
+end
+
+class TriangleError < StandardError
 end
