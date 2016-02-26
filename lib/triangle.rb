6b5136f17e1ddde366c37db160adcfef4255d2ae
JayThaGreat <maskdbandit@gmail.com>
2016-02-26 01:38:56 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  @@sides = []
+  @@type = nil
+
+  def initialize(side1,side2,side3)
+    @@sides = [side1,side2,side3]
+
+  end
+
+  def kind
+    case
+      when @@sides.any? {|side| side <= 0} || @@sides.inject(0){|sum,item| sum + item }-@@sides.max <= @@sides.max
+        begin
+          raise TriangleError
+        end
+      when @@sides.uniq.length == 2
+        @@type = :isosceles
+      when @@sides.uniq.length == 1
+        @@type = :equilateral
+      else
+        @@type = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+
 end
