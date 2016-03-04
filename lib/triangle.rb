0d459e499e71fe5f3b140851df2351ce7c84dd8b
depippo <jdepippo@gmail.com>
2016-03-03 22:17:30 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if self.valid_triangle == false
+      raise TriangleError
+    else
+      if a == b && a == c && b == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  def valid_triangle
+    (a + b > c) && (a + c > b) && (b + c > a) && a > 0 && b > 0 && c > 0
+  end
+
+end
+
+class TriangleError < StandardError
+  "Invalid side lengths for a triangle."
 end
