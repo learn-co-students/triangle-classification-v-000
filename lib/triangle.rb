a6a970e0e6ed18d75ca011d0a05dd77543e3a765
Chris Haugen <haugenc@gmail.com>
2016-03-08 03:40:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,12 +1,30 @@
+require 'pry'
 class Triangle
-  attr_accessor :side1, :side2, :side3
-  
-  def initialize(side1:,side2:,side3:)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  attr_accessor :sides
+
+  def initialize(side1,side2,side3)
+    @sides = []
+    @sides << side1
+    @sides << side2
+    @sides << side3
+    @sides.sort!
   end
   def kind
-    
+    if (sides[0] + sides[1]) <= sides[2] || sides[0] <= 0
+      raise TriangleError
+    elsif sides[0] == sides[2]
+      :equilateral
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "Triangle Error: Please check the sides."
   end
 end
