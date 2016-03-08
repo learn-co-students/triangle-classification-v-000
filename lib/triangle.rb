b3db6c0c6812b30e12a3fd4fe962677bc47f737a
maireadmurphy <mairead.aw.murphy@gmail.com>
2016-03-08 12:58:33 -0500

    added errors

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+attr_accessor :length_1, :length_2, :length_3
+
+ def initialize(length_1, length_2, length_3)
+  @length_1 = length_1
+  @length_2 = length_2
+  @length_3 = length_3
 end
+
+def kind
+  if @length_3 <= 0 || @length_2 <= 0 || @length_1 <= 0
+    raise TriangleError
+  elsif @length_1 + @length_2 <= @length_3 || @length_1 + @length_3 <= @length_2 || @length_3 + @length_2 <= @length_1
+    raise TriangleError
+  elsif @length_2 == @length_1 && @length_2 == @length_3
+    :equilateral
+  elsif @length_1 != @length_2 && @length_1 != @length_3 && @length_3 != @length_2
+    :scalene
+  else
+    :isosceles
+  end
+end
+end
+
+class TriangleError < StandardError
+def message
+  "Triangle Error"
+end
+  end
