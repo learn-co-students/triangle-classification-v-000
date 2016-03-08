eb55475895e84cec573b12906449c2c25cdc6da2
Chris Haugen <haugenc@gmail.com>
2016-03-08 01:49:01 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1:,side2:,side3:)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  def kind
+    
+  end
 end
