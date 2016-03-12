89c359a69e2eab0c505b3c8fb6e87b2a3562ad4d
jbok4 <jbok4@aol.com>
2016-03-12 18:53:04 -0500

    none passing yet, but triangles set up

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if length1 > 0 && length2 > 0 && length3 > 0
+      if length1 == length2 && length1 == length3 && length2 == length3
+       `:equilateral`
+     elsif length1 == length2 || length1 == length3 || length2 == length3 
+       `:isosceles`  
+     else length1 != length2 && length1 != length3 && length2 != length3 
+        `:scalene` 
+      end
+    else 
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    end
+  end
 end
+
+class TriangleError < StandardError
+  def message 
+    "you must give the get_married method an argument of an instance of the person class!"
+  end
+end  
