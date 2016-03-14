eecbd6c72b30854dde1dbaa17c41236ad662a33e
tmharkins <tmharkins@gmail.com>
2016-03-14 01:27:02 -0500

    raise error

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :tri_len_array
+  def initialize(side_1, side_2, side_3)
+  	@tri_len_array = [side_1, side_2, side_3]
+  end
+
+  def kind
+    tri_len_array.sort!
+    raise TriangleError if (tri_len_array[0] + tri_len_array[1]) <= tri_len_array[2]
+		case tri_len_array.uniq.length
+			when 1
+			  return :equilateral
+			when 2
+			  return :isosceles
+			else
+				return :scalene
+		end
+  end
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
