7c1d1feb3433b70c252005b1aeead97b5765dab8
CodeByLine <yleventhal@ymail.com>
2016-03-15 05:30:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Triangle
   attr_accessor :triangles, :equilateral, :isosceles, :scalene, :side1, :side2, :side3, :sides #:type,
 
   def initialize(side1, side2, side3)
-#     sides = {side1:, side2:, side3:}
-     @type = type
+   # binding.pry
+     #sides = {side1:, side2:, side3:}
+     #@type = type
      @side1 = side1
      @side2 = side2
      @side3 = side3
@@ -22,38 +24,60 @@ class Triangle
 #   end
 
   def kind
-   
-    if side1 == side2 == side3
+#     if [side1, side2, side3].uniq?
+#       return :scalene
+#     elsif side1 == side2 and side2 == side3
+# #       self = equilateral
+#       return :equilateral
+#     else
+#       return :isosceles
+#     end
+
+
+#     if side1 || side2 || side3 < 0
+#       begin
+#           raise TriangleError
+#       rescue TriangleError => error
+#           puts error.message
+#      end
+   if side1 <= 0 || side2 <= 0 || side3 <= 0
+       raise TriangleError
+   elsif (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2
+       raise TriangleError
+   elsif side1 == side2 and side2 == side3
       #self = equilateral
-      :quilateral
+      return :equilateral
     elsif
-      side1 == side2 and side1 !== side3
+      side1 == side2 || side2 == side3 || side1 == side3
+      #side1 == side2 and side1 != side3 || side2 == side3 and side1 != side3
      # self = isosceles
-      :isosceles
+      return :isosceles
     else
       #self = scalene
-      :scalene
+      return :scalene
     end
+
+
   end
 end #Triangle class
 
-  class Equilateral < Triangle
-    def equilateral
-    side1 == side2 == side3
-    end
-  end
+#   class Equilateral < Triangle
+#     def equilateral
+#     side1 == side2 == side3
+#     end
+#   end
 
-  class Isosceles < Triangle
-    def isosceles
-    side1 == side2 and side1 !== side3
-    end
-  end
+#   class Isosceles < Triangle
+#     def isosceles
+#     side1 == side2 and side1 !== side3
+#     end
+#   end
 
-  class Scalene < Triangle
-    def scalene
-      side1 != side2 != side3
-    end
-  end
+#   class Scalene < Triangle
+#     def scalene
+#       side1 != side2 != side3
+#     end
+#   end
 
 
 class TriangleError < StandardError
