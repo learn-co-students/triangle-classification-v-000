206304459d2450b48197939a988005f13214f639
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-14 21:15:57 -1000

    All tests passing

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+
+    if @side1 == 0 && @side2 == 0 && @side3 == 0
+      raise TriangleError
+    elsif @side1 < 0 || @side2 < 0 || @side3 < 0
+      raise TriangleError
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side3 + @side2 <= @side1)
+      raise TriangleError
+    end
+
+    if @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
 end
+
+class TriangleError < StandardError
+  
+
+end
+
+
+
+
