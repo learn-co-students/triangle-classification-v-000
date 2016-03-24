763a340d234cccf0db1f197114a81d6aa79f1dc0
Seth Lachman <lachmanseth17@gmail.com>
2016-03-24 11:45:34 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+
+
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    end
+
+    def kind
+    if length1 <= 0 || length2 <= 0 || length3 <= 0
+        begin
+          raise TriangleError
+        end
+      elsif length1 + length2 <= length3 || length2 + length3 <= length1 || length1 + length3 <= length2 
+        begin
+          raise TriangleError
+        end
+elsif length1 != length2 && length2 != length3 && length1 != length3
+       return :scalene
+     elsif
+       length1 == length2 && length2 == length3
+       return :equilateral
+     elsif length1 == length2 || length2 == length3 || length1 == length3
+       return :isosceles
+
+       else
+         false
+       end
+     end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "invalid triangle: please look up the definition of a triangle."
+  end
 end
