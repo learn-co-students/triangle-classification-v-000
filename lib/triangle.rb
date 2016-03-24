bb4f6b8376e3455dccb18f3a442f547792de9952
tddycks <tadzio.smith@gmail.com>
2016-03-24 13:11:23 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  
+  def initialize(a,b,c)
+   @a = a
+   @b = b
+   @c = c
+  end
+
+
+  def kind
+    if not_valid?
+      raise TriangleError
+    
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif (@a == @b) || (@a == @c) || (@b == @c)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+  def not_valid?
+    [a,b,c].min <= 0 || (@a + @b <= @c || @a + @c <= @b || @b + @c <= @a)
+  end
+
+end
+
+
+class TriangleError < StandardError
+
 end
