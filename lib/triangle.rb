092d73642760b197eb51c35b8ed2894c22ab8a48
McN33ly <rmcneely23@gmail.com>
2016-03-26 00:40:29 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,19 +19,18 @@ class Triangle
 
   def kind
     triangle = [@side_1, @side_2, @side_3].sort
-    if @side_1 == @side_2 && @side_1 == @side_3
-      :equilateral
-    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
-      :isosceles
-    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
-      :scalene
-    else
-      triangle[0] + triangle[1] < triangle[2] || (@side_1 < 1 || @side_2 < 1 || @side_3 < 1)
+    if triangle[0] + triangle[1] < triangle[2] || (@side_1 < 1 || @side_2 < 1 || @side_3 < 1)
         begin
           raise TriangleError
         rescue TriangleError => error
           puts error.message
         end
+    elsif @side_1 == @side_2 && @side_1 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      :isosceles
+    elsif @side_1 != @side_2 && @side_1 != @side_3 && @side_2 != @side_3
+      :scalene
     end
   end
 
