ba1d4dbfdb103931fd6420de4b611822234802e0
SiamKing <thesiameseelectric@gmail.com>
2016-03-26 19:13:39 -0700

    dangle that triangle

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    #let x, y, z be sorted lengths
+    x, y, z = [@a, @b, @c].sort
+    raise TriangleError if [@a,@b,@c].min <= 0 || x + y <= z
+    if @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
