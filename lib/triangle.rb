706ccf0acda8a262a896e6ced283f36fa2a87e93
lukegrecki <lukegrecki@gmail.com>
2016-04-07 14:18:58 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def triangle_inequality_unsatisfied?
+    (side_1 + side_2 <= side_3) || (side_2 + side_3 <= side_1) || (side_3 + side_1 <= side_2)
+  end
+
+  def kind
+    if (side_1 <= 0) || (side_2 <= 0) || (side_3 <= 0) || triangle_inequality_unsatisfied?
+      raise TriangleError
+    elsif (side_1 == side_2) && (side_2 == side_3)
+      :equilateral
+    elsif (side_1 == side_2) || (side_2 == side_3) || (side_1 == side_3)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
