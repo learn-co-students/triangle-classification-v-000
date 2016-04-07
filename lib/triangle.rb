4e267d848ceb576015abcca0144bf9713ca7f0a6
yuriyvinnicov <yuriyvinnicov@gmail.com>
2016-04-07 03:00:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+
+  attr_accessor :first_side_length, :second_side_length, :third_side_length
+
+  def initialize(first_side_length,second_side_length,third_side_length)
+    @first_side_length = first_side_length
+    @second_side_length = second_side_length
+    @third_side_length
+  end
+ 
 end
