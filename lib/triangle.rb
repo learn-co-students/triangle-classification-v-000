9b9c71ebddff585b292265466d6352e74d88c729
eliyooy <ebshimon@gmail.com>
2016-04-11 22:18:33 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :left, :right, :bottom
+  
+  def initialize(left, right, bottom)
+    @left = left
+    @right = right
+    @bottom = bottom
+  end
+  
+  def kind
+    if (left + right) <= bottom || (left + bottom) <= right || (right + bottom) <= left || left <= 0 || right <= 0 || bottom <= 0
+      raise TriangleError
+      #begin
+      #  raise TriangleError
+      #rescue TriangleError => error
+      #  puts error.message
+      #end
+    elsif left == right && left == bottom
+      return :equilateral
+    elsif (left == right && left != bottom) || (left == bottom && left != right) || (right == bottom && bottom != left)
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "The triangle as defined has invalid size specifications."
+  end
 end
