822cd93c12446fec0b8cd5259187a6fa9b34a348
hangloos <ericloos00@gmail.com>
2016-05-03 14:52:41 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+attr_accessor :side_one,:side_two,:side_three
+
+def initialize(side_one, side_two, side_three)
+  @side_one = side_one
+  @side_two = side_two
+  @side_three = side_three
 end
+
+
+def kind
+ if side_one <= 0 || side_two <= 0 || side_three <= 0 || side_one + side_two <= side_three || side_one + side_three <=side_two || side_two + side_three <= side_one
+  begin
+    raise TriangleError
+  
+  end
+
+ elsif side_one == side_two && side_one == side_three
+  return :equilateral
+elsif side_two == side_three || side_one == side_two || side_one == side_three
+  return :isosceles
+elsif side_one != side_two && side_one != side_three && side_two != side_three
+  return :scalene
+end
+end
+end
+
+
+class TriangleError < StandardError
+ 
+ def message
+"Your triangle is not valid. Please enter a valid triangle"
+end
+
+end
+
