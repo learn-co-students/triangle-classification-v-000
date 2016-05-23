f37067a9f334eaf242e88b0bb0b04e4d8881d7c4
emill3000 <emill3000@yahoo.com>
2016-05-23 09:55:13 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+  	@s1 = s1
+  	@s2 = s2
+  	@s3 = s3
+  end
+
+  def kind
+  	if (s1+s2>s3 && s2+s3>s1 && s1+s3>s2) == false
+  		raise TriangleError
+  		end
+  	else 
+  		if s1 == s2 && s1 == s3
+  			:equilateral
+  		elsif s1 == s2 || s1 == s3 || s2 == s3
+  			:isosceles
+  		else 
+  			:scalene
+  		end
+  	end	
+end	
+
+class TriangleError < StandardError
+	
 end
+
+
+
