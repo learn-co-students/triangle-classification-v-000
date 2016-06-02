022fb9a471c9def534a3be92b7e3aea9af6e8218
Being Young <beingy@gmail.com>
2016-06-02 19:13:48 -0400

    Done.

---
+++ lib/triangle-stevefrost.rb
@@ -0,0 +1,52 @@
+class Triangle
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    # self.send("#{method_name}=", value)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if zeros || negatives || inequality
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif @side2 == @side3 || @side1 == @side2 || @side1 == @side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def zeros
+    @sides.any? { |side| side == 0 }
+  end
+
+  def negatives
+    @sides.any? { |side| side < 0 }
+  end
+
+  def inequality
+    @side1 + @side2 <= @side3 ||
+    @side1 + @side3 <= @side2 ||
+    @side2 + @side3 <= @side1
+  end
+
+end
+
+  # def equilateral
+  #   @side1 == @side2 && @side2 == @side3
+  #   :equilateral
+  # end
+  #
+  # def isosceles
+  #   @side2 == @side3
+  #   :isosceles
+  # end
+
+class TriangleError < StandardError
+end
