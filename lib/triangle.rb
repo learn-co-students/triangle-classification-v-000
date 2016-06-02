1b2bb91f0a0401cdc789060681cc38a911980475
Being Young <beingy@gmail.com>
2016-06-02 19:03:42 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,48 +1,16 @@
-require 'pry'
-
 class Triangle
-  attr_accessor :equilateral, :isosceles, :scalene
   attr_reader :sides
 
   def initialize(side1, side2, side3)
-    # case
-    # when side1 <= 0 || side2 <= 0 || side3 <= 0
-    #   begin
-    #     raise TriangleError
-    #   rescue TriangleError => error
-    #     error.message1
-    #   end
-    # when side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1
-    #   begin
-    #     raise TriangleError
-    #   rescue TriangleError => error
-    #     error.message2
-    #   end
-    # else
     @sides = [side1, side2, side3]
-    # end
   end
 
   def kind
     case
-    # when !@sides
-    #   begin
-    #     raise TriangleError
-    #   rescue TriangleError => error
-    #     error.message3
-    #   end
     when @sides[0] <= 0 || @sides[1] <= 0 || @sides [2] <= 0
-      # begin
-        raise TriangleError
-      # rescue TriangleError => error
-      #   error.message1
-      # end
+      raise TriangleError
     when @sides[0] + @sides[1] <= @sides[2] || @sides[0] + @sides[2] <= @sides[1] || @sides[1] + @sides[2] <= @sides[0]
-      # begin
-        raise TriangleError
-      # rescue TriangleError => error
-      #   error.message2
-      # end
+      raise TriangleError
     when @sides[0] == @sides[1] && @sides[0] == @sides[2]
       :equilateral
     when @sides[0] == @sides[1] && @sides[0] != @sides[2]
@@ -58,20 +26,4 @@ class Triangle
 end
 
 class TriangleError < StandardError
-
-  def message1
-    "Any sides of the triangle can not be 0 length."
-  end
-
-  def message2
-    "The sume of any two sides of the triangle must be greater than the third side of the triangle."
-  end
-
-  def message3
-    "Triangle has no size or illegal!"
-  end
-
 end
-
-
-# Triangle.new(0,0,0).kind
