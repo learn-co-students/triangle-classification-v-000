7252e6e909b8bd5cc0e07ea985b3ec9b05197b1c
Dom McKellar <dom.mckellar@gmail.com>
2016-06-03 12:34:36 -0700

    Refactor

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,33 @@
-require "pry"
 class Triangle
-  attr_reader :equilateral, :isosceles, :scalene, :side1, :side2, :side3, :all_sides
+  attr_reader :side1, :side2, :side3, :all_sides
 
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @all_sides = [side1,side2,side3]
-  end
-
-  def equilateral
-    # return true if all sides are of equal length
-    all_sides.uniq.size == 1
-  end
-
-  def isosceles
-    # return true if 2 sides are of equal length
-    all_sides.uniq.size == 2
-  end
-
-  def scalene
-    # return true if all sides are of unequal length
-    all_sides.uniq.size == 3
+    @all_sides = [side1, side2, side3]
   end
 
   def triangle?
-    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1 && all_sides.all? { |side| side > 0 }
+    side1 + side2 > side3 &&
+    side1 + side3 > side2 &&
+    side2 + side3 > side1 &&
+    all_sides.all? { |side| side > 0 }
   end
 
   def kind
-    if !triangle?
+    case true
+    when !triangle?
+      # raise error when not a triangle
       raise TriangleError
-    elsif equilateral
-       :equilateral
-    elsif isosceles
+    when all_sides.uniq.size == 1
+      # true if all sides are of equal length
+      :equilateral
+    when all_sides.uniq.size == 2
+      # true if 2 sides are of equal length
       :isosceles
-    else scalene
+    when all_sides.uniq.size == 3
+      # true if all sides are of unequal length
       :scalene
     end
   end
