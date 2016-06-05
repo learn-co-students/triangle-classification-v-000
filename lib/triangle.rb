ea1369f4e3224474bcbe60a4a05f1e03d4027db4
Jonathan Stone <jon_p_stone@yahoo.com>
2016-06-04 20:10:03 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,5 @@
 class Triangle
-  attr_accessor :s1, :s2, :s3, :equilateral,
-  :isosceles, :scalene
+  attr_accessor :s1, :s2, :s3
 
   def initialize(s1, s2, s3)
     @s1 = s1
@@ -9,22 +8,24 @@ class Triangle
   end
 
   def kind
-    if s1 > 0 && s2 > 0 && s3 > 0
-      if s1 == s2 && s2 == s3
-        equilateral
-      elsif (s1 == s2 && s1 != s3) ||
-        (s3 == s1 && s3 != s2) ||
-        (s2 == s3 && s2 != s1)
-        isosceles
-      elsif s1 != s2 && s2 != s3 && s3 != s1
-        scalene
-      end
-    else
-      raise TriangleError
+    validate
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif (s1 == s2 && s1 != s3) ||
+      (s3 == s1 && s3 != s2) ||
+      (s2 == s3 && s2 != s1)
+      :isosceles
+    elsif s1 != s2 && s2 != s3 && s3 != s1
+      :scalene
     end
   end
 
-  class TriangleError < StandardError
+  def validate
+    real_triangle = [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+    [s1, s2, s3].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
+end
 
+class TriangleError < StandardError
 end
