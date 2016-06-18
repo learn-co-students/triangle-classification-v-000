2b7e35a95b4e8d3b133372770eb586b21a719363
alice <github email address>
2016-06-17 23:45:43 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+
+  def kind
+
+    if [@a, @b, @c].min <= 0
+      raise TriangleError
+    elsif @a + @b <= @c || @b + @c <= @a || @c + @a <= @b
+      raise TriangleError
+    else
+      case [@a, @b, @c].uniq.length
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else :scalene
+      end
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
