c8c750ebf556137fb6f32c9aa67dc26985b61594
zszalas2 <zszalas@gmail.com>
2016-06-22 17:00:17 -0400

    For Ash

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    triangle_valid
+    triangle_kind
+  end
+
+  def triangle_valid
+    if (a + b <= c) || (a + c <= b) || (b + c <= a)
+      raise TriangleError
+    end
+    if a == 0 || b == 0 || c == 0
+      raise TriangleError
+    end
+  end
+
+  def triangle_kind
+    if a == b && a == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+        
+
+class TriangleError < StandardError
 end
