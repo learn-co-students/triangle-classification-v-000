54bb8df5fe82f46022f27d4d6678c93322d506a9
Adam Taitano <adam.taitano@gmail.com>
2016-07-14 18:17:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,26 +1,31 @@
 class Triangle
-  attr_accessor :type
+  attr_accessor :type :length_1, :length_2, :length_3
 
   def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    validate_triangle
     if length_1 == length_2 && length_1 == length_3
       @type = :equilateral
     elsif length_2 == length_3 || length_1 == length_3 || length_1 == length_2
       @type = :isosceles
-    elsif length_1 <= 0 || length_2 <= 0 || length_3 <= 0
-      begin
-        raise TriangleError
-      rescue TriangleError
-      end
     else
       @type = :scalene
     end
-  end
-
-  def kind
     @type
   end
 
-  class TriangleError < StandardError
-
+  def validate_triangle
+    real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
   end
+end
+
+
+class TriangleError < StandardError
 end
