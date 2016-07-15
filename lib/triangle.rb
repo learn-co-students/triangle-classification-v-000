b04811279992c669a1fa2c1c36bc0534e7d80432
peterberwind <peterberwind@gmail.com>
2016-07-15 19:00:09 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if [a,b,c].any? {|x| x <= 0}
+      raise TriangleError, "Sides must have positive length"
+    end
+
+    sides = [a,b,c].sort
+
+    unless sides[0]+sides[1] > sides[2]
+      raise TriangleError, "Does not satisfy triangle inequality"
+    end
+
+    if a == b and b == c
+      :equilateral
+    elsif a == b or b == c or a == c
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
