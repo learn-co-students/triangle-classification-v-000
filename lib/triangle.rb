8efd62976358406ed3d7343d4616bddb8097372d
Lenn Hypolite <lenntriniman@hotmail.com>
2016-07-16 02:33:12 +0000

    Done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,3 @@
-require "pry"
 class Triangle
 
   attr_accessor :length_1, :length_2, :length_3
@@ -9,50 +8,33 @@ class Triangle
     @length_3 = length_3
   end
 
-  # def in_valid
-  #   @length_1 + @length_2 < @length_3 || @length_2 + @length_3 < @length_1 || @length_1 + @length_3 < @length_2 && @length_1 < 0 || @length_2 < 0 || @length_3 < 0 && @length_1 % 1 != 0 || @length_2 % 1 == 0 || @length_3 % 1 == 0
-  # end
-  # #
-  # def kind
-  #     if in_valid
-  #        raise TriangleError
-  #     elsif @length_1 == @length_2 && @length_2 == @length_3 && @length_1 == @length_3
-  #       :equilateral
-  #     elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
-  #       :isosceles
-  #     elsif @length_1 != @length_2 && @length_2 != @length_3 && @length_3 != @length_1 && @length_3 != @length_2
-  #       :scalene
-  #       # binding.pry
-  #     end
-  # end # end of method
-  def in_valid
-    array = []
-    array.push(@length_1 , @length_2, @length_3)
-    array.each_with_index do |length, index|
+  def valid # check for inequaity & and each side should be larger than 0
+     length_1 > 0 && length_2 > 0 && length_3 > 0 &&  (length_1 + length_2) > length_3 &&  (length_2 + length_3) > length_1 &&  (length_1 + length_3) >  length_2
+
+  end
+
+  def triangle_names
+      # if sides are equal
+      if @length_1 == @length_2 && @length_2 == @length_3 && @length_1 == @length_3
+        :equilateral
+      # if all three sides are unequal
+      elsif @length_1 == @length_2 || @length_1 == @length_3 || @length_2 == @length_3
+        :isosceles
+      # if any two sides are equal
+      elsif @length_1 != @length_2 && @length_2 != @length_3 && @length_3 != @length_1 && @length_3 != @length_2
+        :scalene
+      end
   end
 
   def kind
-
+    if valid
+      triangle_names
+    else
+      raise TriangleError
+    end
   end
 
-end # end of class
-
-class TriangleError < StandardError
 end
-# array = []
-# array.push(@length_1 , @length_2, @length_3)
-# array.each_with_index do |length, index|
-#   if length == length[index]
-#     :equilateral
-#   elsif
-#       length[index] == length[index]
-#     :isosceles
-#   elsif
-#     length[index] != length[index]
-#     :scalene
-#   elsif length = 0 || length < 0 || length[index] + length[index] < length[index]
-#     begin
-#       raise TriangleError # change the error message here
-#     rescue TriangleError => error #continue after error has been made
-#     end
-#   end
+
+class TriangleError < StandardError # custom error class
+end
