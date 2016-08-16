0e11b66eb6a406a42b348936c7f5ae4ea16c23ac
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-08-16 01:51:41 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+#require "pry"
 class Triangle
-  # write code here
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+  def kind
+    #binding.pry
+    if @l1 + @l2 <= @l3 || @l3 + @l2 <= @l1 || @l3 + @l1 <= @l2
+        raise TriangleError
+    elsif @l1 == @l2 && @l1 == @l3
+      :equilateral
+    elsif @l2 == @l3 || @l1 == @l3 || @l1 == @l2
+      :isosceles
+    elsif @l1 != @l2 && @l3 != @l2 && @l3 != @l1
+      :scalene
+
+
+    end
+
+
+
+
+
+  end
+
 end
+
+class TriangleError < StandardError
+  def message
+    "Sorry Invalid Triangle"
+  end
+end
+
+#x = Triangle.new(2,4,2)
+#puts x.kind
