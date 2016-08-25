1d7f003ab50d64977eedea6308c968aad68fd350
jinstrider2000 <efrainperezjr@live.com>
2016-08-24 20:30:01 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
   # write code here
+  def initialize(*sides)
+    @sides = []
+    for index in 0..2 do
+      @sides[index] = sides[index]
+    end
+  end
+
+  def kind
+    if !((@sides[0] + @sides[1]) > @sides[2] && (@sides[1] + @sides[2]) > @sides[0] && (@sides[0] + @sides[2]) > @sides[1]) || @sides.any? { |side| side < 0 }
+      raise TriangleError 
+    elsif @sides[0] == @sides[1] && @sides[0] == @sides[2]
+      :equilateral
+    elsif @sides[0] == @sides[1] || @sides[1] == @sides[2] || @sides[0] == @sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "The sides lengths provided do not constitue a triangle."
+  end
 end
