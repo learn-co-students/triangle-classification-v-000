94764a787b9af233b71bc74f8953e96cc0afad00
Clare <clare.e.horton@gmail.com>
2016-09-08 04:29:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+    attr_accessor:sides
+    
+    
+  def initialize(side_1, side_2, side_3)
+      @sides = [side_1, side_2, side_3]
+      @sides.sort!
+  end
+  
+  def kind
+    if sides.any? {|side| side <= 0}
+            raise TriangleError
+    elsif (sides[0] + sides[1]) <= sides[2]
+            raise TriangleError
+    elsif sides[0] == sides[1] && sides[1] == sides[2]
+        :equilateral
+    elsif sides[0] == sides[1] || sides[1] == sides[2]
+        :isosceles
+    else
+        :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+     def message
+       "Sides must make a valid triangle!"
+     end
 end
