4c8027c4f1428fd847e1856799ce807d598ec64c
Onyekachi <onye@smmaven.com>
2016-09-25 10:02:08 -0700

    TriangleError

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :one, :two, :three
+
+  def initialize(one, two, three)
+  	@one = one
+  	@two = two
+  	@three = three
+  end
+
+  def kind
+  	if @one <= 0 || @two <= 0 || three <= 0
+  		raise TriangleError
+  	elsif @one + @two <= @three || @one + @three <= @two || @two + @three <= @one
+  		raise TriangleError
+  	elsif @one == @two && @two == @three
+  		:equilateral
+  	elsif @one == @two || @one == @three || @two == @three
+  		:isosceles
+  	else
+  		:scalene
+  	end
+  end
+  		
+  		
+  		
+end
+
+class TriangleError < StandardError
 end
