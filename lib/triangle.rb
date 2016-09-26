ceda9ed35cece4e12d6344608b51abb0ced87159
bc86 <michael.4486@gmail.com>
2016-09-25 21:14:48 -0400

    checking for different triangles

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_accessor :left, :right, :bottom
+
+  def initialize(left, right, bottom)
+    @left = left
+    @right = right
+    @bottom = bottom
+  end
+
+  def kind
+    case
+    when left <= 0 || right <= 0 || bottom <= 0 || left >= right + bottom || right >= left + bottom || bottom >= right + left
+        raise TriangleError
+      when @left == @right && @left == @bottom
+          :equilateral
+      when @left == @right || @left == @bottom || @right == @bottom
+          :isosceles
+      when @left != @right && @left != @bottom
+          :scalene
+    end
+  end
+end
+
+
+class TriangleError < StandardError
+
 end
