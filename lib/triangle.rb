96b823fc86748e203ed350c2fa2a8feecbc9f41f
Jon Fougner <jon.fougner@gmail.com>
2016-10-27 15:05:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  attr_accessor :l1, :l2, :l3
+
+  def initialize (l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+  end
+
+  def kind
+    if l1 <= 0 || l2 <= 0 || l3 < 0 || l1 >= l2 + l3 || l2 >= l1 + l3 || l3 >= l1 + l2
+      raise TriangleError
+    end
+
+    if l1 == l2 && l2 == l3
+      return :equilateral
+    elsif l1 == l2 || l1 == l3 || l2 == l3
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+   "The side lengths provided as arguments do not meet the requirements of a triangle."
+ end
 end
