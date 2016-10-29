a88b5f29af371589b5616fcba3992c5aa9564232
Bruno Boehm <bruno.boehm@gmail.com>
2016-10-29 15:16:18 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,12 @@
+require_relative './config/environment'
+
+puts "welcome to Pry"
+
+def reload!
+  load '.lib/triangle.rb'
+end
+
+desc 'A console'
+task :console do
+  Pry.start
+end
--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    if [side1, side2, side3].any? { |e| e <= 0 }
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if [@side1, @side2, @side3].uniq.size == 1
+        :equilateral
+    elsif self.violates?
+        raise TriangleError
+    elsif [@side1, @side2, @side3].uniq.size == 2
+        :isosceles
+    else
+        :scalene
+    end
+  end
+
+  def violates?
+    sorted = [@side1, @side2, @side3].sort
+    true if sorted[0] + sorted[1] <= sorted[2]
+  end
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
