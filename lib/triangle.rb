d4c01db760e40eac4b156e04590df1e50287ca9a
derek maloney <derekjmaloney@gmail.com>
2016-11-14 14:43:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
 class Triangle
-  # write code here
+
+  def initialize(side_one, side_two, side_three)
+    @one = side_one
+    @two = side_two
+    @three = side_three
+  end
+
+  def valid
+    if @one > 0 && @two > 0 && @three > 0 && (@one + @two) > @three && (@one + @three) > @two && (@two + @three) > @one
+      return true
+    else
+      return false
+    end
+  end
+
+
+  def kind
+    if @one <= 0 || @two <= 0 || @three <= 0 || (@one + @two) <= @three || (@one + @three) <= @two || (@two + @three) <= @one
+
+        raise TriangleError
+
+
+    elsif @one == @two && @two == @three
+        return :equilateral
+    elsif (@one == @two && @two != @three) || (@one == @three && @three != @two) || (@two == @three && @three != @one)
+        return :isosceles
+    else
+        return :scalene
+      end
+    end
+  
+
+
+end
+
+
+class TriangleError < StandardError
+  def message
+    "bad triangle."
+  end
 end
