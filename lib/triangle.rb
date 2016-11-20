2c6660c268009ccb4d0b4a10e0967eba1e7daa9b
Nicholas Ratti <nickratti@gmail.com>
2016-11-20 18:58:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,10 +5,14 @@ class Triangle
     @length_a = length_a
     @length_b = length_b
     @length_c = length_c
+    triangle = [@length_a,@length_b,@length_c].sort
+    binding.pry
   end
 
   def kind
     if @length_a + @length_b > @length_c && @length_a + @length_c > @length_b && @length_b + @length_c > @length_a && @length_a > 0 && @length_b > 0 && @length_c > 0
+
+
       if [@length_a,@length_b,@length_c].uniq.count == 1
         :equilateral
       elsif [@length_a,@length_b,@length_c].uniq.count == 2
