7b317cbe1b67b56d0b8b80c661596367b75a3a3f
Sean Clarke <xseanclarke@gmail.com>
2016-12-12 20:07:45 +0000

    Done.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -40,6 +40,7 @@ describe 'Triangle' do
     expect(Triangle.new(0.4, 0.6, 0.3).kind).to eq(:scalene)
   end
 
+  # Bonus Tests
   it 'knows that triangles with no size are illegal' do
     expect{Triangle.new(0, 0, 0).kind}.to raise_error(TriangleError)
   end
