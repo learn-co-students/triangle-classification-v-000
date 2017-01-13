3e2c2c148d2369d91fea5bafc258c5feec5deb60
Jonathan Foster <redcabinny@gmail.com>
2017-01-13 04:41:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,5 @@
 class Triangle
-  
+
   def initialize(side1, side2, side3)
   	@side1 = side1
   	@side2 = side2
@@ -10,7 +10,7 @@ class Triangle
   	side_array = [@side1, @side2, @side3].sort
   	if side_array.any? { | side | side <= 0 } || (side_array[0] + side_array[1] <= side_array[2])
   		raise TriangleError
-  	elsif @side1 == @side2 && @side2 == @side3
+  	elsif side_array.uniq.length == 1
   		:equilateral
   	elsif side_array == side_array.uniq
   		:scalene
