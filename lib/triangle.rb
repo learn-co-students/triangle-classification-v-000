fca8d9f04916d5943f9b271d14207f41066ea70f
Jonathan Foster <redcabinny@gmail.com>
2017-01-13 03:05:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+  	@side1 = side1
+  	@side2 = side2
+  	@side3 = side3
+  end
+
+  def kind
+  	side_array = [@side1, @side2, @side3].sort
+  	if side_array.any? { | side | side <= 0 } || (side_array[0] + side_array[1] <= side_array[2])
+  		raise TriangleError
+  	elsif @side1 == @side2 && @side2 == @side3
+  		:equilateral
+  	elsif side_array == side_array.uniq
+  		:scalene
+  	elsif side_array.uniq.length == 2
+  		:isosceles
+  	end
+  end
+
+end
+
+class TriangleError < StandardError
 end
