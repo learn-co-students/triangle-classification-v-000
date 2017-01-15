a8e47f4920c655f12f0aafe5c4addd29a1320814
Ryan Evans <ryanwevans@icloud.com>
2017-01-14 19:32:24 -0800

    in progress

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if ((@length1 || @length2 || @length3) <= 0) || (@length1 < (@length2 + @length3)) || (@length2 < (@length1 + @length3)) || (@length3 < (@length1 + @length2))
+      begin
+        raise TriangleError
+      rescue TriangleError
+      end
+    else
+      if (@length1 != @length2) && (@length1 != @length3) && (@length2 != @length3)
+        return :scalene
+      elsif (@length1 == @length2) && (@length1 == @length3) && (@length2 == @length3)
+        return :equilateral
+      else
+        # (@length1 == @length2) || (@length1 == @length3) || (@length2 == @length3)
+        return :isosceles
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
+  puts "Invalid triangle!"
 end
