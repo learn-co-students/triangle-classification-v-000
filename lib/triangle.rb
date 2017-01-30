b9207a068e7e8c11ff5f94778fda9d9dfb78e4ce
Kevin Adkins <kevinladkins@gmail.com>
2017-01-30 20:06:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-    if (@a =< 0) || (@b =< 0) || (@c =< 0) ||((@a + @b) < @c) || ((@a + @c) < @b) || ((@b + @c) < @a)
+    if (@a <= 0) || (@b <= 0) || (@c <= 0) ||((@a + @b) <= @c) || ((@a + @c) <= @b) || ((@b + @c) <= @a)
       raise TriangleError
     elsif
       (@a == @b) && (@b == @c)
