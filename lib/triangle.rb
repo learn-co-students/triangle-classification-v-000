bca78f8ffec7f336841bca94375448387c2c9b41
Michael Djurdjevic <mdjurdj1@binghamton.edu>
2017-02-04 06:19:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,46 @@
+require 'pry'
+
 class Triangle
   # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1 
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def illegal_triangle?
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2 
+      true
+    elsif @side1 <= 0 || @side2 <= 0 || @side3 <= 0
+      true
+    else
+      false
+    end
+  end
+  
+  def kind
+    if self.illegal_triangle?
+    #  begin
+        raise TriangleError 
+    #  rescue TriangleError => error
+    #    puts error.message
+    #  end
+    elsif (@side1 == @side2 && @side2 == @side3)
+      :equilateral 
+    elsif (@side1 == @side2 || @side2 == @side3 || @side1 == @side3)
+      :isosceles 
+    else
+      :scalene
+    end
+  end
 end
+
+class TriangleError < StandardError
+  def message
+    "You input an invalid triangle! Check your side measurements, and try again."
+  end
+end
+
+
