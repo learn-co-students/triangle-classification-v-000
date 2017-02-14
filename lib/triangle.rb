101f790faa1a2ea7d7d8cbf49afa835d675edd40
Pepper <axpepper@gmail.com>
2017-02-13 21:34:46 -0800

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
   # write code here
+
+  def initialize(s1, s2 ,s3)
+  	@s1= s1
+  	@s2= s2
+  	@s3= s3
+  end
+
+  def kind
+  	if (@s1<=0 || @s2<=0 || @s3<=0) || [@s1,@s2,@s3].map{|t| (@s1+@s2+@s3 - t) <= t}.any? 
+  		raise TriangleError
+  	elsif @s1 == @s2 && @s2 == @s3
+  		return :equilateral
+  	elsif @s1 == @s2 || @s2 == @s3 || @s1 == @s3
+  		return :isosceles
+  	else
+  		return :scalene
+  	end
+  end
+
+end
+
+class TriangleError < StandardError
 end
