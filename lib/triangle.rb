ced4066f4b0b4727f7301720f41d0d4530e437f7
Parv Kumar <pkumar@trinity.edu>
2017-02-14 01:24:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+    @list = [@length1, @length2, @length3]
+
+  end
+
+  def kind
+    @list.each do |length|
+      if length == 0 || length < 0
+        raise TriangleError
+      end
+    end
+    if (@length1 + @length2) <= @length3 || (@length2 + @length3) <= @length1 || (@length1 + @length3) <= @length2
+      raise TriangleError
+    end
+    if @length1 == @length2 && @length1 == @length3
+      return :equilateral
+    end
+    if @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+      return :isosceles
+    end
+    if @length1 != @length2
+      if @length1 != @length3
+        if @length2 != @length3
+          return :scalene
+        end
+      end
+    end
+
+
+  end
+
+
+end
+
+class TriangleError < StandardError
 end
