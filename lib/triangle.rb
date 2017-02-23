64d288c1b6ef931e5e00fa9a64c4cdfc5d19b984
Jenna Francis <jef399@nyu.edu>
2017-02-23 20:20:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,18 +20,6 @@ class Triangle
       :scalene
     end
   end
-  # def get_married(person)
-  #     self.partner = person
-  #     if person.class != Person
-  #       begin
-  #         raise PartnerError
-  #       rescue PartnerError => error
-  #           puts error.message
-  #       end
-  #     else
-  #       person.partner = self
-  #     end
-  # end
 end
 
 class TriangleError < StandardError
