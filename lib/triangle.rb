490bb2c355c4677b4d1f1d20fa51f7c6b059a3e7
Art Rodriguez <art.rodriguez@live.com>
2017-02-24 19:19:55 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z
+  
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+  
+  def kind
+    if @x <= 0 || @y <= 0 || @z <= 0
+      raise TriangleError
+    elsif @x + @y <= @z || @y + @z <= @x || @z + @x <= @y
+      raise TriangleError
+    elsif @x == @y && @y == @z
+      :equilateral
+    elsif @x == @y || @y == @z || @z == @x
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
