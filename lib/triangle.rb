d4b763cdc568c18bafd1fa6088f07c8c877cf21e
charlie lee <ctlee9096@gmail.com>
2017-03-16 03:24:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,4 +1,4 @@
-require "pry"
+#require "pry"
 class Triangle
    attr_accessor :length1, :length2, :length3
   def initialize(length1, length2, length3)# write code here
@@ -15,6 +15,18 @@ class Triangle
     #rescue TriangleError => error
     #  puts error.message
     #end
+    if [@length1, @length2, @length3].any? do |x| x<=0 end #enum boolean method =
+      #if any? of code block evaluates to True, raise an error
+      raise TriangleError
+    end
+    sides = [@length1,@length2,@length3].sort
+    #sorts numeric value in ascending order
+    unless sides[0]+sides[1]>sides[2]
+      #Unless is opposite of If stmt construction
+      #Unless (sum of 2 sides IS GREATER (A.K.A.
+      #SAME AS ALWAYS EXCEEDS) than 3rd side stmt)
+      raise TriangleError
+    end
     if @length1 == @length2 && @length1 == @length3  && @length2 == @length3
     #binding.pry
     :equilateral
@@ -22,23 +34,24 @@ class Triangle
     :isosceles
     elsif @length1 != @length3 && @length1 != @length2
     :scalene
-    else #@length1 == 0 && @length2 == 0  && @length3 == 0
-      #begin
-        raise TriangleError
-      #rescue TriangleError => error
-        #puts error.message
-      #end
+    #else #@length1 == 0 && @length2 == 0  && @length3 == 0
+    #  begin
+    #    raise TriangleError
+    #  rescue TriangleError => error
+    #    puts error.message
+    #  end
     end #length1 == 0 && length2 == 0 && length3 == 0
   end
 end
 class TriangleError < StandardError
-  #attr_accessor :length1, :length2, :length3
-  #def initialize
-  #  @length1 = length1
-  #  @length2 = length2
-  #  @length3 = length3
-  #end
-  def message
-    "Not a valid triangle"
-  end
+    #attr_accessor :length1, :length2, :length3
+    #def initialize
+    #  @length1 = length1
+    #  @length2 = length2
+    #  @length3 = length3
+    #end
+    def message
+      "Not a valid triangle"
+    end
 end
+#end
