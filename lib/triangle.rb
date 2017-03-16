6bfd55e14f783bf16e916a64fe7a73598d1ef80c
Michael Etlinger <mk.etlinger@gmail.com>
2017-03-16 18:58:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles, :scalene
+
+  def initialize(a, b, c)
+    @a_side = a
+    @b_side = b
+    @c_side = c
+
+    total_sum = @a_side + @b_side + @c_side
+    raise TriangleError if total_sum <= 0
+  end
+
+  def kind
+    a_b_sum = @a_side + @b_side
+    a_c_sum = @a_side + @c_side
+    b_c_sum = @b_side + @c_side
+
+    if a_b_sum > @c_side && a_c_sum > @b_side && b_c_sum > @a_side
+      case
+      when @a_side == @b_side && @a_side == @c_side && @b_side == @c_side
+        :equilateral
+      when @a_side != @b_side && @a_side != @c_side && @b_side != @c_side
+        :scalene
+      else
+        :isosceles
+      end
+    else
+    raise TriangleError
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
