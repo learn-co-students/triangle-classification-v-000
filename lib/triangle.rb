e2fa64e274c3b6b0cb5ed82d7402b84a76bb4e3f
Jarrel deLottinville <jarrel.delottinville@gmail.com>
2017-03-25 05:04:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_reader :triangle
+
+  def initialize(x, y, z)
+    @triangle = [x, y, z].sort
+  end
+
+  def kind
+    if triangle[0] <= 0
+      raise TriangleError, "Each side length must be greater than 0"
+    elsif  triangle[0] + triangle[1] <= triangle[2]
+      raise TriangleError, "Invalid side lengths - violates triangle inequality principle"
+    else
+      case triangle.uniq.size
+        when 1 then :equilateral
+        when 2 then :isosceles
+        else :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
