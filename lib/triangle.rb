e73cadff9a498d36326d68b7010087bd417cfeba
github username <github email address>
2017-03-31 14:15:42 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,52 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(a, b, c)
+    @side_a = a
+    @side_b = b
+    @side_c = c
+  end
+
+  def kind
+    if validate_side_lengths
+      if @side_a == @side_b && @side_a == @side_c
+        :equilateral
+      elsif @side_a == @side_b || @side_a == @side_c || @side_b == @side_c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  def validate_side_lengths
+    unless @side_a > 0 && @side_b > 0 && @side_c > 0 && validate_traingle_ineq
+      begin
+        raise TriangleError
+      end
+    else
+      true
+    end
+  end
+
+  def validate_traingle_ineq
+    if @side_a + @side_b <= @side_c
+      false
+    elsif @side_a + @side_c <= @side_b
+      false
+    elsif @side_b + @side_c <= @side_a
+      false
+    else
+      true
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
+  # def message
+  #   "The sides you entered do not form a valid traingle."
+  # end
+
 end
