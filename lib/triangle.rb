dc7ca4679dddd2917aa62697003d0bcefe55a73e
Bryan Hou <bryan.hou94@gmail.com>
2017-04-03 18:04:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+  def initialize(l1,l2,l3)
+    @lengths= [l1,l2,l3]
+    @lengths.sort!
+  end
+
+  def kind
+    if @lengths[0]+@lengths[1] <= @lengths[2] 
+      raise TriangleError
+    elsif @lengths[0] == @lengths[2]
+      :equilateral
+    elsif @lengths[0] == @lengths[1] || @lengths[1] == @lengths[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
