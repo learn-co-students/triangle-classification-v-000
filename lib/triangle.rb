d71159e9a87625f4fa859699ada6b448ec84917f
Terry Blue <terryblue@comcast.net>
2017-04-06 02:46:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  @@type = ["equilateral", "isosceles", "scalene"]
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if ((@side1 + @side2) <= @side3 || (@side2 + @side3) <= @side1 || (@side1 + @side3) <= @side2) ||
+      (@side1 == 0 && @side2 == 0 and @side3 == 0)
+      raise TriangleError
+    end
+
+    if @side1 == @side2 && @side2 == @side3
+      @@type[0].to_sym
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      @@type[1].to_sym
+    elsif @side1 != @side2 || @side2 != @side3 || @side1 != @side3
+      @@type[2].to_sym
+    else
+      puts "oops!"
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    puts "Invalid triangle!"
+  end
 end
