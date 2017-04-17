57ff18f9808fc68930dc34088381a03bb3cb20bf
Eva Navarror <emanao@me.com>
2017-04-17 17:38:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,33 @@
+require "pry"
 class Triangle
-  attr_accessor :a_length, :b_length, :c_length
- def initialize(a,b,c)
-   @a_length = a_length
-   @b_length = b_length
-   @c_length = c_length
+  attr_accessor :side_a, :side_b, :side_c
+ def initialize(side_a, side_b, side_c)
+   @side_a = side_a
+   @side_b = side_b
+   @side_c = side_c
  end
  def kind
    kind=nil
-   if (a_length == b_length == c_length)
+   if (side_a<=0 || side_b<=0 || side_c<=0) ||
+     (side_a+side_b<=side_c || side_a+side_c<=side_b || side_b+side_c<=side_a)
+     raise TriangleError
+
+   elsif (side_a == side_b && side_a == side_c)
      kind = :"equilateral"
-   elif
-     kind = :"next"
+     #binding.pry
+   elsif (side_a == side_b || side_a == side_c || side_b == side_c)
+     kind = :"isosceles"
+   elsif (side_a != side_b && side_a != side_c && side_b != side_c)
+     kind = :"scalene"
+   else
+
    end
+
  end
+
+end
+class TriangleError < StandardError
+  def message
+    "The sum of the lengths of any two sides of a triangle always exceeds the length of the third side"
+  end
 end
