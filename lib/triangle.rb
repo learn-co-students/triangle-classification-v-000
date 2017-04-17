99ac99468ccb3b9d8e4c6914246fcd5e7cb109c2
Eva Navarror <emanao@me.com>
2017-04-17 14:25:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+  attr_accessor :a_length, :b_length, :c_length
+ def initialize(a,b,c)
+   @a_length = a_length
+   @b_length = b_length
+   @c_length = c_length
+ end
+ def kind
+   kind=nil
+   if (a_length == b_length == c_length)
+     kind = :"equilateral"
+   elif
+     kind = :"next"
+   end
+ end
 end
