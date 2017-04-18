0abe608aa3d3f718d496f63ceebd75afcd2a6a4e
irina <iafanasyevaa@gmail.com>
2017-04-19 01:39:49 +0300

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Triangle
-  # write code here
+
+  attr_reader :all_sides
+
+  def initialize(a, b, c)
+    @all_sides = [] #trying to abstract validation with private methods
+    @all_sides << a
+    @all_sides << b
+    @all_sides << c
+  end
+
+  def kind
+    if check_side_length? || check_summ_of_sides?
+      begin
+        raise TriangleError
+      end
+    elsif self.all_sides.uniq.length == 1
+      :equilateral
+    elsif self.all_sides.uniq.length != self.all_sides.length
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+  def check_summ_of_sides? #trying to abstract
+    self.all_sides.collect.with_index.any? do |value, i|
+      value >= self.all_sides.reject.with_index {|v, j| j == i}.inject(0, :+)
+    end
+  end
+
+  def check_side_length? #trying to abstract
+    self.all_sides.any? {|value| value <= 0}
+  end
+end
+
+class TriangleError < StandardError
 end
