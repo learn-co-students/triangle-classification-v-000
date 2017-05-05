0e546a8056535c40bf441b0c7c1a6c4edaedf3b7
Dave Schlauderaff <dschlaud@gmail.com>
2017-05-04 21:06:11 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require "pry"
+
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    # binding.pry
+    self.side_a = side_a
+    self.side_b = side_b
+    self.side_c = side_c
+  end
+
+  def kind
+    if side_a <= 0 || side_b <= 0 || side_c <= 0
+        raise TriangleError
+    elsif side_a + side_b <= side_c || side_a + side_c <= side_b || side_b + side_c <= side_a
+        raise TriangleError
+    elsif side_a == side_b && side_a == side_c
+      return :equilateral
+    elsif side_a == side_b || side_a == side_c || side_b == side_c
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+
+  def message
+    "all sides of a triangle must have a length greater than zero"
+
+  end
+
 end
