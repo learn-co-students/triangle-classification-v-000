6a29975934646973494fd2b53a038a051eaa1223
hyoyou <heatherhyou@gmail.com>
2017-05-09 21:17:20 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,20 @@
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    raise TriangleError if [a,b,c].min <= 0
+    x, y, z = [a,b,c].sort
+    raise TriangleError if x + y <= z
+    [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
+  end
+end
+
+class TriangleError < StandardError
 end
