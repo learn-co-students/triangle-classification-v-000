75887476ed632a32cee0c2afca08c6a88365ac73
hyoyou <heatherhyou@gmail.com>
2017-05-10 10:19:01 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,23 @@ class Triangle
   end
 
   def kind
+#    if a == b && b == c
+#      :equilateral
+#    elsif a == b && b !=c || b == c && c != a || c == a && a != b
+#      :isosceles
+#    elsif a != b && b != c && c != a
+#      :scalene
+
     raise TriangleError if [a,b,c].min <= 0
     x, y, z = [a,b,c].sort
     raise TriangleError if x + y <= z
     [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
-   end
+
+#    raise TriangleError if [a,b,c].min <= 0
+#    x, y, z = [a,b,c].sort
+#    raise TriangleError if x + y <= z
+#    [:equilateral,:isosceles,:scalene].fetch([a,b,c].uniq.size - 1)
+  end
 end
 
 class TriangleError < StandardError
