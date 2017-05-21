3580cc47efcb424792b39cfa87e6a26edb6b313b
Menachem Hornbacher <me@mendel.tech>
2017-05-21 16:20:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
   # write code here
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+  end
+  def kind
+    @sides.each{ |side| raise TriangleError if side <= 0 }
+    raise TriangleError if @sides[0] >= @sides[1] + @sides[2]
+    raise TriangleError if @sides[1] >= @sides[0] + @sides[2]
+    raise TriangleError if @sides[2] >= @sides[0] + @sides[1]
+
+    # puts "#{@sides[0]} #{@sides[1] + @sides[2]}"
+    # puts "#{@sides[1]} #{@sides[0] + @sides[2]}"
+    # puts "#{@sides[2]} #{@sides[0] + @sides[1]}"
+
+    type = :equilateral if @sides.uniq.count == 1
+    type = :isosceles if @sides.uniq.count == 2
+    type = :scalene if @sides.uniq.count == 3
+    type
+  end
+end
+
+class TriangleError < StandardError
 end
