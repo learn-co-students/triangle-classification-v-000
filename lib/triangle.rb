4fbe3b67f9214f0e11f6c917fe67ebaa58c3fa68
Rebecca Bratu <beckybratu@gmail.com>
2017-06-08 03:52:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,35 +1,35 @@
 class Triangle
-  attr_accessor :a, :b, :c
-  attr_reader :type 
+  attr_accessor :a, :b, :c, :type
 
   def initialize(a, b, c)
-    @a = a 
-    @b = b 
-    @c = c 
+    @a = a
+    @b = b
+    @c = c
     @type = type
   end
 
-  def kind      
+  def kind
     if @a == @b && @a == @c && @b == @c && !illegal_triangle?
       @type = :equilateral
     elsif (@a == @b || @a == @c || @b == @c) && !illegal_triangle?
       @type = :isosceles
     elsif @a != @b && @a != @c && @b != @c && !illegal_triangle?
-      @type = :scalene 
-    else 
+      @type = :scalene
+    else
         raise TriangleError
-    end 
+    end
   end
 
   def illegal_triangle?
-    if (@a <= 0 || @b <= 0 || @c <= 0) || ((@a + @b) <= @c || (@a + @c) <= @b || (@a + @c) <= @b)
+    if (@a <= 0 || @b <= 0 || @c <= 0) || ((@a + @b) <= @c || (@a + @c) <= @b || (@b + @c) <= @a)
+      true
     end
-  end 
-  
+  end
+
 end
 
 class TriangleError < StandardError
-  def message 
+  def message
         "Yo, that's an illegal triangle."
-  end 
+  end
 end
