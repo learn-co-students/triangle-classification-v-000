205bb4b59bfb22933fc1a6c787aa95b3b2edc1de
Seb Gertz <sebgertz@gmail.com>
2017-06-25 18:09:16 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+  def kind(a,b,c)
+    if a => 1 && b >=1 && c >=1 && a + b > c && a +c > b && c + B > a
+      if a == b && == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      elsif a != b && b!=c && a != c
+        :scalene
+      end
+    else
+      begin
+      raise TriangleError
+    end
+  end
+
+
+class TriangleError < StandardError
+ # triangle error code
 end
