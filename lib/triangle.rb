3444fbdf8c07e1402360ecbafdd7a786d7fb7399
Seb Gertz <sebgertz@gmail.com>
2017-06-25 19:42:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,15 +4,16 @@ class Triangle
     @a = a
     @b = b
     @c = c
+    @sides = [a,b,c]
   end
 
   def kind
-    if @a >= 0 && @b >=0 && @c >=0 && @a + @b > @c && @a + @c > @b && @c + @b > @a
+    if real_triangle?
       if @a == @b && @b == @c
         :equilateral
       elsif @a == @b || @a == @c || @b == @c
         :isosceles
-      else 
+      else
         :scalene
       end
     else
@@ -22,9 +23,17 @@ class Triangle
     end
   end
 
+  def real_triangle?
+    sides_sum = @sides.reduce(:+)
+    if @sides.any?{|side| side <= 0} || @sides.any?{|side| (sides_sum-side) <= side}
+      false
+    else
+      true
+    end
+  end
+
 end
 
-
 class TriangleError < StandardError
- # triangle error code
+
 end
