1daf6b5e92982c5a5ae657d0f6651db08b37e3d0
Sohee Cho <soheecho94@gmail.com>
2017-07-07 03:31:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+  attr_accessor :x, :y, :z, :sides
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+    @sides = [@x, @y, @z]
+  end
+
+  def equilateral
+    @x == @y && @x == @z
+  end
+
+  def isosceles
+    ((@x == @y) && (@x + @y) > @z) || ((@y == @z) && (@y + @z) > @x )|| ((@z == @x) && (@z + @x) > @y)
+  end
+
+  def scalene
+    @x != @y && @y != @z && @z != @x
+  end
+
+  def illegal
+    @sides.any? {|side| side <= 0} || ((@x + @y) <= @z) || ((@y + @z) <= @x) || ((@z + @x) <= @y)
+  end
+
+  def kind
+    if self.illegal
+    begin
+      raise TriangleError
+      end
+    elsif self.equilateral
+      :equilateral
+    elsif self.isosceles
+      :isosceles
+    elsif self.scalene
+      :scalene
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
