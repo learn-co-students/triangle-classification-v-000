7d20f432f068283e2ab286ca23dd76b7d712ed3b
hansenjl <jenn.leigh.hansen@gmail.com>
2017-07-10 16:34:16 -0700

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if side1+side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1
+        raise TriangleError
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
+      raise TriangleError
+    elsif side1 == side2 && side1 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "you created a triangle inequality"
+  end
 end
