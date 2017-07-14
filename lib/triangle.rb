20c608fc171c56e74ccbd7d03065862ff5f62abf
Robert Peckham <peckham.rob@gmail.com>
2017-07-14 13:42:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,18 +9,25 @@ class Triangle
   end
 
   def kind
+      validate_triangle
       if l1 == l2 && l2== l3
         :equilateral
-      elsif a == b || b == c || a === c
+      elsif l1 == l2 || l2 == l3 || l1 === l3
         :isosceles
       else
         :scalene
     end
   end
+
+  def validate_triangle
+    actual_triangle = [(l1 + l2 > l3), (l1 + l3 > l2), (l2 + l3 > l1)]
+    [l1, l2, l3].each {|side| actual_triangle << false if side <= 0}
+    raise TriangleError if actual_triangle.include?(false)
+  end
 end
 
 class TriangleError < StandardError
-  def message
-    "That's not a triangle dummy!"
-  end
+  # def message
+  #   "That's not a triangle dummy!"
+  # end
 end
