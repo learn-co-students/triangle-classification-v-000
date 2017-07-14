95b45911771cac9cbec58d02c0def25bd248a242
Robert Peckham <peckham.rob@gmail.com>
2017-07-14 11:10:31 +0000

    figure out proofs

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :l1, :l2, :l3
+
+  def initialize(l1, l2, l3)
+    @l1 = l1
+    @l2 = l2
+    @l3 = l3
+
+  end
+
+  def kind
+      if l1 == l2 && l2== l3
+        :equilateral
+      elsif a == b || b == c || a === c
+        :isosceles
+      else
+        :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "That's not a triangle dummy!"
+  end
 end
