8819a28d2fa1bd2f84bb477062449633d0215476
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-08-05 18:26:20 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :a,:b,:c
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+def kind
+      if a == b && b == c
+        return :equilateral
+      elsif a == b || b == c || a == c
+        return :isosceles
+      else a != b && b != c && c != a
+        return :scalene
+      end
 end
+ 
+ 
+end
+  
+class TriangleError < StandardError
+end
+
+
+
