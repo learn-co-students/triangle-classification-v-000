23b6f35325500839853992e88ab281528fbfae08
churchwerks <steven.lennis@icloud.com>
2017-08-21 11:43:08 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    validate_triangle
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    real_triangle = [(side1 + side2 > side3), (side1 + side3 > side2), (side2 + side3 > side1)]
+    [side1, side2, side3].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+end
+
+
+class TriangleError < StandardError
+ #attr_accessor :some_object, :some_operation
+ #def initialize(object, operation)
+ #@some_object, @some_operation = object, operation
+ #super("Failed to perform #{operation} on #{object}")
+ #end
 end
