58a6eb329f221d7c8f1f35235551c7cb39eacbfd
Karen Berba <kberba@gmail.com>
2017-08-25 03:24:01 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,18 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
