9f5bdd9b8efd259b6eb147e849db5f4e9373e148
Mary Benitez <meebenitez@gmail.com>
2017-08-29 17:33:08 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+
+  attr_accessor :first_length, :second_length, :third_length
+
+  def initialize (first_length, second_length, third_length)
+    @first_length = first_length
+    @second_length = second_length
+    @third_length = third_length
+  end
+
+  def kind
+    if illegal_check_zero(self.first_length, self.second_length, self.third_length) || illegal_check_inequality(self.first_length, self.second_length, self.third_length)
+    #  begin
+        raise TriangleError
+    #  rescue TriangleError => error
+    #    puts error.message
+    #  end
+    else
+      if self.first_length == self.second_length && self.second_length == self.third_length
+        :equilateral
+      elsif self.first_length != self.second_length && self.second_length != self.third_length && self.third_length != self.first_length
+        :scalene
+      else
+        :isosceles
+      end
+    end
+  end
 end
+
+def illegal_check_zero (first_length, second_length, third_length)
+  [first_length, second_length, third_length].all? { |length| length <= 0 }
+end
+
+def illegal_check_inequality (first_length, second_length, third_length)
+  (self.first_length + self.second_length) <= self.third_length || (self.second_length + self.third_length) <= self.first_length || (self.first_length + self.third_length) <= self.second_length
+end
+
+
+
+
+
+
+
+  class TriangleError < StandardError
+
+    def message
+      "The lengths given do not make a valid triangle."
+    end
+
+  end
