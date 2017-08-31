36c43cbe5143e6874094ab3b3b6f7dc330fe75f1
Guesly  Germain <germain1211@outlook.com>
2017-08-31 17:12:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    sides = [side_1, side_2, side_3].sort
+    #
+    # raise TriangleError if sides.first <= 0 || sides[2] >= sides[1] + sides[0]
+    # return :equilateral if sides.uniq.length  == 1
+    # return :isosceles if sides.uniq.length  == 2
+    # :scalene
+
+
+    if sides.any? {|x| x <= 0} || sides[0] + sides[1] <= sides[2]
+      raise TriangleError
+    elsif side_1 == side_2 && side_2 == side_3
+      :equilateral
+    elsif side_1 == side_2 || side_2 == side_3 || side_1 == side_3
+      :isosceles
+    else side_1 != side_2 && side_2 != side_3 && side_3 != side_1
+      :scalene
+    end
+
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "This is an invalid triangle!! Please try agian :P"
+  end
 end
