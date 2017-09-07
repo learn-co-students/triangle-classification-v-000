1deab220a0bce0a80a89725e82aea3fa4700d0b2
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-09-07 22:05:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+
+  def kind
+    if a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    end
+    if ((a + b <= c) || (a + c <= b) || (b + c <= a))
+      raise TriangleError
+    end
+    if a == b && b == c
+      :equilateral
+    elsif  a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+
+end
+
+class TriangleError < StandardError
 end
