076e800e738fd91b79f0adc5d0fa163b908534c8
Johan Esteves <jsesteves91@gmail.com>
2017-09-12 01:46:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,61 @@
-class Triangle
+class TriangleError < StandardError
+  def message
+    "This is not an triangle!"
+  end
+end
+
+class Triangle < TriangleError
   # write code here
+  attr_accessor :side1, :side2, :side3, :equilateral, :isosceles, :scalene
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+
+    @sides = []
+    @sides << side1 << side2 << side3
+  end
+
+  def kind
+    if valid? && inequality?
+      if equilateral?
+        :equilateral
+      elsif isosceles?
+        :isosceles
+      elsif scalene?
+        :scalene
+      end
+
+    else
+      raise TriangleError
+    end
+
+  end
+
+  def valid?
+    self.sides.all? {|side| side > 0}
+    #self.side1 > 0 && self.side2 > 0 && side.side3
+  end
+
+  def equilateral?
+    self.side1 == self.side2 && self.side2 == self.side3
+  end
+
+  def isosceles?
+    self.sides.any? { |side| self.sides.drop(sides.find_index(side)+1).include?(side)  }
+  end
+
+  def scalene?
+    self.sides.none? { |side| self.sides.drop(sides.find_index(side)+1).include?(side)  }
+  end
+
+  def inequality?
+    self.side1 + self.side2 > self.side3 && self.side2 + self.side3 > self.side1 && self.side1 + self.side3 > self.side2
+  end
+
+  def sides
+    @sides
+  end
+
 end
