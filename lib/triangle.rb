4ac7d978748d657730746cc8177529ed7df0af54
Hunter Kleindl <hkleindl@gmail.com>
2017-09-14 23:04:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if [@a, @b, @c].any? {|i| i <= 0} || @a + @b <= @c || @a + @c <= @b || @b + @c <= @a
+      begin
+        raise TriangleError
+      end
+    elsif @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Impossibruu!"
+  end
 end
