860fbe25f77ee18b626a41835ee4c04f32bee8ec
Bret Burau <burhooo@gmail.com>
2017-09-17 12:16:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :first, :second, :third
+
+  def initialize(first, second, third)
+    @first = first
+    @second = second
+    @third = third
+  end
+
+  def kind
+    if @first <= 0 || @second <= 0 || @third <= 0 || inequity?
+      raise TriangleError
+    elsif @first == @second && @first == @third
+      response = :equilateral
+    elsif @first == @second || @first == @third || @second == @third
+      response = :isosceles
+    elsif !(@first == @second || @first == @third || @second == @third)
+      response = :scalene
+    end
+    response
+  end
+end
+
+def inequity?
+  answer = false
+  if !(@first + @second > @third)
+    answer = true
+  elsif !(@first + @third > @second)
+    answer = true
+  elsif !(@second + @third > @first)
+    answer = true
+  end
+
+end
+
+class TriangleError < StandardError
+
 end
