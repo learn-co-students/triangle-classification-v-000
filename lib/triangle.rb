0afdba2960133ef3fdce0a2689cf34c5a3236005
Susan Fowler <infopro94@gmail.com>
2017-09-19 16:57:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -20,13 +20,12 @@ class Triangle
 end
 
 class TriangleError < StandardError
-    def triangle(a,b,c) # raise TriangleError if @a < 0 || @b < 0 || @c < 0 || @a + @b <= @c
-    sides = [a, b, c].sort
-    raise TriangleError, "Invalid size #{sides[0]}" unless sides[0] > 0
-    raise TriangleError, "Impossible triangle" if sides[0] + sides[1] <= sides[2]
-    raise TriangleError, "Illegal triangle, sides cannot be negative" if sides[0..2] < 0
-    return [:scalene, :isosceles, :equilateral][ 3 - sides.uniq.size ]
-  #sum of 2 sides must exceed length of 3rd
-#   #each side must be >0
-    end
-end
+
+    def triangle(a, b, c)
+       sides = a, b, c
+       raise TriangleError, "Illegal triangle, sides must be greater than zero" if a <= 0
+       raise TriangleError, "Illegal triangle, sides cannot be negative" unless sides > 0
+       raise TriangleError, "Illegal triangle, violates triangle inequality" if a + b <= c
+     end
+
+     end
