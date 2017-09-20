039b57b1485682fb722d2bcb3562ae382ef41f8d
Susan Fowler <infopro94@gmail.com>
2017-09-20 16:10:20 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,28 +4,29 @@ class Triangle
       @a = a
       @b = b
       @c = c
-      #more code
     end
 
     def kind
-      if @a == @b && @a == @c
-        :equilateral
-      elsif @a == @b || @a == @c || @b == @c
-        :isosceles
-      else
-        :scalene
-      end
-    end
+      # sides = @a, @b, @c
 
+      if @a <= 0 || @b <= 0 || @c <= 0 #addresses no size and negative size
+          raise TriangleError,   "Illegal triangle, sides must be greater than zero"
+        end
+
+      if @a + @b <= @c || @b + @c <= @a || @c + @a <= @b #addresses inequality rule
+          raise TriangleError, "Illegal triangle, violates triangle inequality"
+        end
+
+        if @a == @b && @a == @c
+          :equilateral
+        elsif @a == @b || @a == @c || @b == @c
+          :isosceles
+        else
+          :scalene
+      end
+     end
 end
 
 class TriangleError < StandardError
 
-    def triangle(a, b, c)
-       sides = a, b, c
-       raise TriangleError, "Illegal triangle, sides must be greater than zero" if a <= 0
-       raise TriangleError, "Illegal triangle, sides cannot be negative" unless sides > 0
-       raise TriangleError, "Illegal triangle, violates triangle inequality" if a + b <= c
-     end
-
-     end
+end
