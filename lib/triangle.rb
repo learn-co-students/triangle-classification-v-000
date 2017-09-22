a77f8d7e216b648a91943d15e301866a6002b447
ndalcin <dalcin.nicole@gmail.com>
2017-09-22 16:35:02 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+    attr_accessor :length1, :length2, :length3, :equilateral, :isosceles, :scalene
+
+    def initialize(length1, length2, length3)
+      @length1 = length1
+      @length2 = length2
+      @length3 = length3
+    end
+
+    def kind
+      if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+          raise TriangleError
+      elsif @length1 + @length2 <= @length3 || @length2 + @length3 <= @length1 || @length3 + @length1 <= @length2
+          raise TriangleError
+      elsif @length1 == @length2 && @length2 == @length3
+        :equilateral
+      elsif @length2 == @length1 || @length2 == @length3 || @length1 == @length3
+        :isosceles
+      elsif @length1 != @length2 && @length2 != @length3
+        :scalene
+      end
+    end
+
+end
+
+class TriangleError < StandardError
+   def message
+     puts "This is an illegal triangle!"
+   end
 end
