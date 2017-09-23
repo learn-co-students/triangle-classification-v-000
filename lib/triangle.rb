2862cd7741248deb6772d6bf238eb7d65446e988
Sara Stanton <sara.mpa13@gmail.com>
2017-09-23 02:30:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
+
+  attr_accessor :one, :two, :three, :type
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    if one + two <= three || two + three <= one || one + three <= two || one <= 0 || two <= 0 || three <= 0
+      raise TriangleError
+    else
+      if one == two && two == three
+        :equilateral
+      elsif one != two && two != three && one != three
+        :scalene
+      elsif one == two || two == three || one == three
+        :isosceles
+      end
+    end
+  end
+
+end
+
+class TriangleError < StandardError
 end
