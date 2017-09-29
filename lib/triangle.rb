42d96c88986ce89a6413c438fd719dd0a9ce9a18
Dasha Nikitina <dashadariallc@gmail.com>
2017-09-29 10:06:07 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
   # write code here
-end
+  attr_accessor :a, :b, :c
+
+    def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    end
+
+
+  def kind
+    raise TriangleError if @a <= 0 || @b <= 0 || @c <= 0
+    raise TriangleError if @a + @b <= @c || @b + @c <= @a || @c + @a <= @b
+        if @a == @b && @b == @c
+          :equilateral
+        elsif @a == @b || @b == @c ||@c == @a
+          :isosceles
+        else
+          :scalene
+
+      end
+    end
+  end
+
+    class TriangleError < StandardError
+     # triangle error code"
+     "illegal triangle"
+    end
