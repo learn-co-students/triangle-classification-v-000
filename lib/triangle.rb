edb12938e13188899a8c66fcb557a2888c5ada83
ronald vilorio <soccer23415@gmail.com>
2017-10-02 16:55:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
+
   # write code here
+
+  def initialize(a_length,b_length,c_length)
+    @a_length = a_length
+    @b_length = b_length
+    @c_length = c_length
+  end
+  attr_accessor :a_length, :b_length, :c_length
+
+  def kind
+    two_side_total = a_length + b_length
+
+    if a_length <= 0 || b_length <= 0 || c_length <= 0
+      raise TriangleError
+    elsif two_side_total < c_length || c_length + a_length == b_length || c_length + b_length <= a_length
+      raise TriangleError
+    elsif a_length == b_length && b_length == c_length
+      :equilateral
+    elsif a_length == b_length || a_length == c_length || b_length == c_length
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+end
+
+class TriangleError < StandardError
+  def message
+    "triangles with no size are illegal"
+  end
 end
