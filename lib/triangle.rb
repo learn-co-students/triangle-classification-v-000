6c767d59ed6039711eeea3da6cd845f873d68db1
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-10-03 05:46:40 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(side_one, side_two, side_three)
+    @all_sides = [side_one, side_two, side_three]
+    @rotate_one = @all_sides.rotate
+    @rotate_two = @all_sides.rotate(2)
+  end
+
+  def kind
+    result = nil
+    check_inequality = false
+
+    @all_sides.each_index do |index|
+      if @all_sides[index] + @rotate_one[index] <= @rotate_two[index]
+        check_inequality = true
+      end
+    end
+
+    if @all_sides[0] == @all_sides[1] && @all_sides[0] == @all_sides[2]
+      result = :equilateral
+    end
+
+    if @all_sides[0] <= 0 || @all_sides[1] <= 0 || @all_sides[2] <= 0 || check_inequality == true
+      begin
+       raise TriangleError
+      end
+    else
+     result
+    end
+  end # end kind method
+
+end # end class
+
+class TriangleError < StandardError
 end
