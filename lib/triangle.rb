e54bb0e570acd58d1873ee1afe9e9b58ec6ed1ea
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-04 18:43:34 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_the_triangle
+    if   a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+     :scalene
+   end
+  end
+
+
+
+  def validate_the_triangle
+      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each { |s| real_triangle << false if s <= 0 }
+      raise TriangleError if real_triangle.include?(false)
+    end
+end
+
+class TriangleError < StandardError
+  def message
+    "Not so good yet!"
+  end
 end
