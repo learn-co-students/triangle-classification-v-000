9fa60f502283e5d1fbeae99f1533390aa3f219ae
Alejandra Campero <alejandracamperovega@gmail.com>
2017-10-16 22:51:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    legal?
+    if ((a == b) && (b == c))
+      :equilateral
+    elsif ((a == b) || (a == c) || (b == c))
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def legal?
+    if (a <= 0) || (b <= 0) || (c <= 0)
+      raise TriangleError
+    end
+    if (a + b <= c) || (a + c <= b) || (b + c <= a)
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
