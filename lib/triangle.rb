ec029595aab9d0e622725ada09a4b7a602e1ae50
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-27 23:10:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
   # write code here
+ attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a + b > c && b + c > a && c + a > b
+      if a == b && a == c && b == c
+        :equilateral
+      elsif a != b && a != c && b !=c
+        :scalene
+      else
+        :isosceles
+      end
+    else
+       if a <= 0 || b <= 0 || c <= 0 || a + b < c || b + c = a || c + a < b
+         raise TriangleError
+         puts error.message
+      end
+    end
+  end
 end
+
+class TriangleError < StandardError
+  def message
+    "triangles with no size, negative sides or inequality are illegal."
+  end
+end
+
+# shape = Triangle.new(5, 8, 9)
+# shape.kind
