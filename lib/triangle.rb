8949fcd37884afe6d3d0ca26286707fa0d8a2563
Ben Blackman <bblkmn5@gmail.com>
2017-10-27 22:55:58 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+      @a = a
+      @b = b
+      @c = c
+  end
+
+  def kind
+#first makes sure array numbers are of a triangle
+    valid_triangle
+#know that equilateral triangles have equal sides, larger also have equal sides
+    if a == b && b == c
+      :equilateral
+#knows that isosceles triangles have last two, first and last, or first and second sides be equal 
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+#if none of the above, is a scalene 
+      :scalene
+    end
+  end
+
+  def valid_triangle
+    triangle = [(a + b > c), (a + c > b), (b + c > a)]
+#false if sides are less than or equal to zero
+    [a,b,c].each { |side| triangle << false if side <= 0}
+    raise TriangleError if triangle.include?(false)
+  end
+end
+
+class TriangleError < StandardError
 end
