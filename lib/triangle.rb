37e6a57a87ff0abd428a4c44175a3ca23b0a0ca1
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-28 16:44:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    if length_1 == 0 || length_2 == 0 || length_3 == 0 || length_1 + length_2 <= length_3 || length_2 + length_3 <= length_1 || length_1 + length_3 <= length_2
+      raise TriangleError
+    else
+      @length_1 = length_1
+      @length_2 = length_2
+      @length_3 = length_3
+    end
+  end
+
+  def kind
+    if (length_1 == length_2) && (length_2 == length_3)
+      :equilateral
+    elsif ((length_1 == length_2 ) && (length_2 != length_3)) || ((length_2 == length_3) && (length_3 != length_1)) || ((length_1 == length_3) && (length_3 != length_2))
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
