edde39fdfe8d8a8c7759e999437bb8c1cc1382e8
Christopher DiStasio <github email address>
2017-11-16 17:49:23 -0600

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  def initialize(side_one_length, side_two_length, side_three_length)
+    @side_one_length = side_one_length
+    @side_two_length = side_two_length
+    @side_three_length = side_three_length
+  end
+
+  class TriangleError < StandardError
+    
+    def get_married(person)
+    self.partner = person
+    if person.class != Person 
+      raise PartnerError 
+    else
+      person.partner = self
+    end
+  end
+  
+end
+
+    :equilateral
+
+    :isosceles
+
+    :scalene
+
+
 end
