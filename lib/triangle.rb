94c66e42ae0053f4586c01fa3efb3a421da391d7
Olena Ageyeva <eageeva@hotmail.com>
2017-11-18 04:10:48 +0000

    1

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
   # write code here
+  attr_accessor :type
+
+
+
+  def initialize(a,b,c)
+    puts "#{a} #{b} #{c}"
+    if a*b*c==0
+      @type=:illigal
+    else
+      if a==b and b==c
+        @type=:equilateral
+      else
+         if b==c || a==c || a==b
+            @type=:isosceles
+         else
+            @type=:scalene
+         end
+
+      end
+    end
+  end
+
+  def kind
+
+    if @type==:illigal
+      begin
+         raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    else
+      @type
+    end
+  end
+
+  class TriangleError<StandardError
+    def message
+        "wrong triangle"
+    end
+  end
+
 end
