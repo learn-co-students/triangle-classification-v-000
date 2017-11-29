6c54f2a61264dd6cf8acdab02a954101fec2e84f
cboujoukos <cboujoukos@gmail.com>
2017-11-29 16:53:31 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = []
+    @sides << @a
+    @sides << @b
+    @sides << @c
+  end
+
+  def kind
+    confirm_triangle
+    if @a == @b && @a == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def confirm_triangle
+    raise TriangleError unless @sides.sort[2] < (@sides.sort[0] + @sides.sort[1]) && @sides.none?{|side| side <= 0}
+  end
+
 end
+
+
+
+class TriangleError < StandardError;end
