b83606948068da09073f552d7b08922c9fd0fdc5
Jason Vance <jbvance@gmail.com>
2017-11-29 19:41:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if !valid?(@a, @b ,@c)
+      raise TriangleError
+    else
+      get_type(@a, @b, @c)
+    end
+
+  end
+
+  def valid?(a,b,c)
+    return ((a + b > c) && (a + c > b) && (b + c > a) && (b + a > c) && (c + a > b) && (c + b > a) && a > 0 && b > 0 && c > 0)
+  end
+
+  def get_type(a, b, c)
+    return :equilateral if a==b && b==c
+    if a == c || b == c || a == b
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+
+
+end
+
+class TriangleError < StandardError
+  def message
+    "The triangle is not valid"
+  end
 end
