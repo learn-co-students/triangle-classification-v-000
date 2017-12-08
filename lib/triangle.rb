d4eac1ba06a74045bdeac364153cb9690694f97a
Adjoa Darien <adjoa.darien@gmail.com>
2017-12-08 15:47:08 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    self.sides_equal_zero
+    self.triangle_inequality
+    if @a == @b && @b == @c
+      :equilateral
+    elsif @a == @b || @a == @c || @b == @c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_equal_zero
+    if [@a, @b, @c].any?{ |side| side <= 0 }
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  def triangle_inequality
+    scen1 = @a + @b > @c
+    scen2 = @a + @c > @b
+    scen3 = @b + @c > @a
+
+    if [scen1, scen2, scen3].any?{ |scen| scen == false }
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+end
+
+
+class TriangleError < StandardError
+   def message
+     "There's something wrong with this triangle."
+   end
 end
