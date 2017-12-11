9525f9ab38fea25c6a491f190773aaf926c0ffa3
charityyoungblood <youngblood.charity@gmail.com>
2017-12-11 15:56:29 -0800

    added logic to kind method

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,24 +3,39 @@ require 'pry'
 class Triangle
   
   # initialize with 3 arguments (length_1, length_2, length_3 - these will be integers)
+  attr_accessor :length_1, :length_2, :length_3
   def initialize(length_1, length_2, length_3)
-    
+    @length_1 = length_1 
+    @length_2 = length_2
+    @length_3 = length_3
   end 
   
   # create an instance method kind - no arguments 
   def kind
     # - this method will return a symbol of the triangle type 
     # will need to add some sort of logic/if statement to determine if triangle is valid
-    # the sum of two sides of triangle have to be greater than the length of the third side 
+    # the sum of two sides of triangle have to be greater than the length of the third side for triangle to be valid
+    # compare length amounts - then return type of triangle
       # options for calculating length 
         # - length_1 + length_2 
         # - length_2 + length_3 
         # - length_1 + length_3
+    binding.pry
+    if length_1 + length_2 > length_3 
+    elsif length_2 + length_3 > length_1 
+    elsif length_1 + length_3 > length_2
     # - valid triangle types are: 
-      # - :equilateral
-      # - :isosceles
-      # - :scalene
+      # - :equilateral - all three sides are equal
+      if length_1 == length_2 && length_2 == length_3 && length_1 == length_3
+        :equilateral 
+      # - :isosceles - 2 sides are equal
+      elsif length_1 == length_2 || length_2 == length_3 || length_1 == length_3
+        :isosceles
+      # - :scalene - no sides are equal 
+      else
+        :scalene
   # the kind method should raise a custom error - TriangleError
+    end 
   end 
   # write a custom error class, include after the Triangle class 
     # the customer error class should be inherited from StandardError
