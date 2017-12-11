e1fc69e28240f035d04a7993c9718ed34b9d6957
Sharmina Akhtar <sakht318@gmail.com>
2017-12-11 09:11:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,52 @@
 class Triangle
   # write code here
+  attr_accessor :x, :y, :z
+
+  def initialize (x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+
+    #t = (x + y + z) / 2.0
+
+  #the following must be positive to be a valid triangle
+    valid1 = x.positive? && y.positive? && z.positive?
+    valid2 = x + y > z && x + z > y && y + z > x
+
+    if valid1 && valid2
+      true
+    else
+     raise TriangleError
+    end
+    #if valid_triangle == true && valid_triangle2 == true
+     #true
+    #else
+     #raise TriangleError
+    #end
+
+    if x == y && x == z
+      :equilateral
+    elsif x == y || x == z || y == z
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  #def valid_triangle
+  #  x.positive? && y.positive? && z.positive?
+  #end
+
+  #def valid_triangle2
+  #  x + y > z && x + z > y && y + z > x
+  #end
+
+end
+
+
+class TriangleError < StandardError
+ # triangle error code
 end
