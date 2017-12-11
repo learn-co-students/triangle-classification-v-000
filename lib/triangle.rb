1d9b681c55dfa8b88b6a0c47bd20709420ce3412
charityyoungblood <youngblood.charity@gmail.com>
2017-12-11 15:32:00 -0800

    added initialize method placeholder to triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,9 @@
 class Triangle
-  # write code here
+  
   # initialize with 3 arguments (length_1, length_2, length_3 - these will be integers)
+  def initialize(length_1, length_2, length_3)
+    
+  end 
   
   # create an instance method kind 
     # - this method will return a symbol of the triangle type 
