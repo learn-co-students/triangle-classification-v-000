4a001413823185443ae38a39f19324154e515bd1
charityyoungblood <youngblood.charity@gmail.com>
2017-12-11 15:30:32 -0800

    added comments on how to structure code

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
   # write code here
+  # initialize with 3 arguments (length_1, length_2, length_3 - these will be integers)
+  
+  # create an instance method kind 
+    # - this method will return a symbol of the triangle type 
+    # - valid triangle types are: 
+      # - :equilateral
+      # - :isosceles
+      # - :scalene
+  # the kind method should raise a custom error - TriangleError
+  
+  # write a custom error class, include after the Triangle class 
+    # the customer error class should be inherited from StandardError
+  
 end
