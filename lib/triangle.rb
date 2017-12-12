75a1746d6a0738a85af1a71f1f9a44ac672fe2dd
Phoebe Lau <phoebelau7@gmail.com>
2017-12-12 20:51:55 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,50 @@
 class Triangle
   # write code here
+  attr_accessor :x, :y, :z
+  attr_reader :type
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def equilateral?
+    self.x == self.y && self.y == self.z
+  end
+
+  def isosceles?
+    self.x == self.y || self.y == self.z || self.x == self.z
+  end
+
+  def scalene?
+    self.x != self.y && self.y != self.z && self.x != self.z
+  end
+
+  def illegal?
+    if self.x <= 0 || self.y <= 0 || self.z <= 0 || self.x + self.y <= self.z || self.y + self.z <= self.x || self.x + self.z <= self.y
+      true
+    end
+  end
+
+  def kind
+    if illegal?
+        raise TriangleError
+        puts error.message
+    elsif
+      equilateral?
+      @type = :equilateral
+    elsif
+      isosceles?
+      @type = :isosceles
+    elsif
+      scalene?
+      @type = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "Please enter correct lengths."
+  end
 end
