b9baf35fb0439b0d7f0bd1259d53cbc903912875
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 15:13:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one,side_two,side_three)
+    @side_one=side_one
+    @side_two=side_two
+    @side_three=side_three
+  end
+
+  def valid?
+    if @side_one==0 || @side_two==0||  @side_three==0
+    end
+
+  end
+
+  def triangle_type
+    if @side_one==@side_two and @side_two==@side_three
+    end
+  end
+
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
