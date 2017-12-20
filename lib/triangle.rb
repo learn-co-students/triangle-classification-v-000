58540eef421544261d5d36ca36bcdf1338477604
Brittany Morris <brittmmendez@gmail.com>
2017-12-20 16:29:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,19 +7,24 @@ class Triangle
     @side_three=side_three
   end
 
-  def valid?
-    if @side_one==0 || @side_two==0||  @side_three==0
+  def kind
+      if @side_one<=0 || @side_two<=0||  @side_three<=0
+        raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_two + @side_three <= @side_one || @side_one + @side_three <= @side_two
+            raise TriangleError
+    elsif @side_one==@side_two && @side_two==@side_three
+          :equilateral
+    elsif @side_one==@side_two || @side_two==@side_three|| @side_one==@side_three
+          :isosceles
+    else
+        :scalene
+      end
     end
-
   end
 
-  def triangle_type
-    if @side_one==@side_two and @side_two==@side_three
-    end
+  class TriangleError < StandardError
+  def message
+     "This tye of triangle is illegal!"
   end
 
-end
-
-class TriangleError < StandardError
- # triangle error code
-end
+ end
