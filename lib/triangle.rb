657d63fe7a9cf473f3daa3fb9b8bf380380e45a2
Eddie Melo <heriberto@melo.nyc>
2018-01-02 22:43:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,9 @@ class Triangle
   end
 
   def kind
+    #place all the sides into an array and sort the array
     sides = [side1, side2, side3].sort
+    #raise the error if the lowest # is less than or equal to 0 or if the 2 lesser sides are less than or equal to the biggest side
     raise TriangleError if sides[0] <= 0 or sides[0] + sides[1] <= sides[2]
     return :equilateral if sides[0] == sides[2]
     return :isosceles if sides[0] == sides[1] or sides[1] == sides[2]
