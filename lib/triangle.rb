033d6bd0f991ca76b218bc4552a86488c18f5804
John O'Connor <oconnojb@gmail.com>
2018-01-03 20:40:20 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
   # write code here
+  attr_accessor :kind
+
+  def initialize(a, b, c)
+    sides = []
+    sides << a
+    sides << b
+    sides << c
+    sorted = sides.sort
+
+    if a <= 0 || b <= 0 || c <= 0 || (sorted[0]+sorted[1]<=sorted[2])
+      raise TriangleError
+    end
+
+    if a == b && a == c
+      @kind = :equilateral
+    elsif (b==c && !(a==b))||(a==c && !(a==b))||(a==b && !(a==c))
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "This triangle sucks."
+  end
 end
