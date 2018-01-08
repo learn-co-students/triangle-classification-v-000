3c71c67b216d5b54c7dfbc058cd7ec3ba26e5e9d
JOSELY CASTRO <joselycas@gmail.com>
2018-01-08 19:08:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,7 @@ class Triangle
 
   def initialize(l,w,h)
     @l = l
-    @w= w
+    @w = w
     @h = h
   end
 
@@ -14,22 +14,32 @@ class Triangle
     if l == w && w == h
       :equilateral
     elsif l == w || l == h || h == w
-        :isosceles
-      else
-        :scalene
+      :isosceles
+    else
+      :scalene
     end
   end
 
   def validate_triangle
+    #triangle_array = [(a + b > c), (a + c > b), (b + c > a)]
+    #binding.pry
+
+    #iterate over array , check if = < 0 add false and raise error if array includes false
     if l <= 0 || w <= 0 || h <= 0
          raise TriangleError
     end
-    if l == w && w < h
-    raise TriangleError
-  end
-  if l == w && w != h
-    raise TriangleError
-  end
+
+    if (l + w ) <= h
+      raise TriangleError
+    end
+
+    if  (l + h) <= w
+      raise TriangleError
+    end
+
+    if (w + h) <= l
+     raise TriangleError
+    end
   end
 end
 
