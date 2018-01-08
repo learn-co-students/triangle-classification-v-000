bd0da6a1b0daece2fbe1084d6b50aa03f254cbd3
JOSELY CASTRO <joselycas@gmail.com>
2018-01-08 16:34:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require "pry"
 class Triangle
-  # write code here
+ attr_accessor :l, :w, :h
+
+  def initialize(l,w,h)
+    @l = l
+    @w= w
+    @h = h
+  end
+
+  def kind
+    validate_triangle
+    #binding.pry
+    if l == w && w == h
+      :equilateral
+    elsif l == w || l == h || h == w
+        :isosceles
+      else
+        :scalene
+    end
+  end
+
+  def validate_triangle
+    if l <= 0 || w <= 0 || h <= 0
+         raise TriangleError
+    end
+    if l == w && w < h
+    raise TriangleError
+  end
+  if l == w && w != h
+    raise TriangleError
+  end
+  end
+end
+
+class TriangleError < StandardError
+ # triangle error code
 end
