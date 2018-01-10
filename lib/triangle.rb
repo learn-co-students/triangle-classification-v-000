b4b5d4a671d7b41bff174adc9e22196666e2d812
Brad Hambrice <hambrice@uab.edu>
2018-01-10 21:52:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+attr_accessor :first_length, :second_length, :third_length
+  def initialize(first_length, second_length, third_length)
+    @first_length = first_length
+    @second_length = second_length
+    @third_length = third_length
+  end
+  
+  def kind
+    if proper_triangle && greater_than_zero
+      if @first_length == @second_length && @second_length == @third_length
+        :equilateral
+        elsif @first_length == @second_length || @first_length == @third_length || @second_length == @third_length
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+end
+
+def proper_triangle
+ if @first_length + @second_length > @third_length && @first_length + @third_length > @second_length && @second_length + @third_length > @first_length
+   return true 
+ end
+ end
+ 
+ def greater_than_zero
+  if @first_length > 0 && @second_length > 0 && @third_length > 0 
+    return true 
+  end
+ end
+end
+class TriangleError < StandardError
+  
 end
