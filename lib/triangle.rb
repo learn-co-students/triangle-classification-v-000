28b1f8bc5f01dab12445103cdcfda8ace35ce3d5
Connor Gallopo <connor.gallopo@me.com>
2018-01-12 10:31:40 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind 
+    if ((@a + @b <= @c) || (@b + @c <= @a) || (@a + @c <= @b)) || (@a == 0 && @b == 0 && @c == 0)
+      raise TriangleError
+    elsif @a == @b && @b == @c
+      :equilateral
+    elsif (@a == @b || @a == @c) || (@b == @a || @b == @c)
+      :isosceles
+    elsif @a != @b && @b != @c
+      :scalene
+    end
+  end
 end
+class TriangleError < StandardError
+  end
