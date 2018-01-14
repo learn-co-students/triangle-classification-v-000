cf4dd80f74acfdd1631345643a19f93d16593656
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-14 22:32:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    all = [@one,@two,@three]
+    sorted = all.sort
+
+    if (sorted[0] + sorted[1]) <= sorted[2]
+      raise TriangleError
+    else
+      if all.uniq.length == 1
+        return :equilateral
+      elsif all.uniq.length != all.length
+        return :isosceles
+      else
+        return :scalene
+      end
+    end
+
+  end
+
+end
+
+
+class TriangleError < StandardError
+
 end
