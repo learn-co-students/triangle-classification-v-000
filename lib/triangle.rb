d4176c0d37d477518a1f565697498209136b2923
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-01-18 20:38:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    end
+  def kind
+    if (a <= 0) || (b <= 0) || (c <= 0) || (a + b <= c) || (b + c <= a ) || (a + c <= b)
+      raise TriangleError
+    elsif a == b && b == c
+      :equilateral
+    elsif (a == b && a != c) || (a == c && a != b) || (b == c && b != a)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+
+class TriangleError<StandardError
 end
