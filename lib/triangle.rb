b75fe1370db385edc11ea7fd4e8c4fa36fe7b0cb
Connie Hyman <constance.hyman@mchsi.com>
2018-01-27 07:14:43 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c, :kind
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+    @kind = kind
+  end
+
+  def kind
+    if valid_triangle
+      true
+  #  if !valid_triangle
+    else
+  #    !valid_triangle
+      raise TriangleError
+    end
+
+      if a == b && b == c && a == c
+        :equilateral
+      elsif a == b || b == c || a == c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+
+
+  def valid_triangle
+    a > 0 && b > 0 && c > 0 && a + b > c && a + c > b && b + c > a
+  end
+end
+
+
+
+class TriangleError < StandardError
+
+  def message
+    "triangle error code"
+  end
 end
