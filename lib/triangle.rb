225631b43567687231703a3d837fb48da36196e2
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-29 21:32:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,20 +13,21 @@ class Triangle
     if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 || (side_2 + side_3 > side_1) || (side_1 + side_3 < side_2) || (side_1 + side_2 > side_3)
       begin
         raise TriangleError
+      rescue TriangleError => error
+        puts error.message
       end
     elsif side_1 == side_2 && side_1 == side_3 && side_2 == side_3
        :equilateral
      elsif side_2 == side_3 || side_1 == side_3 || side_1 == side_2
        :isosceles
-     elsif side_1 != side_2 != side_3
+     elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
        :scalene
-
+     end
   end
 
   class TriangleError < StandardError
-
+    def message
+      "The sum of the lengths of any two sides of a triangle must exceed the length of the third side. Further, each side must be larger than 0."
+    end
   end
-
-
-
 end
