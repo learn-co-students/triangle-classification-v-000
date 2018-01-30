4507d0428e870ba9754b6bc1eb2e014ab1356419
Robert Hughes <roberts.ginger.email@gmail.com>
2018-01-30 04:19:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_reader :kind
+
+  def initialize(x, y, z)
+    if no_size?([x, y, z])
+      raise TriangleError
+    elsif inequality_broken?([x, y, z])
+      raise TriangleError
+    elsif [x, y, z].uniq.length == 1
+      @kind = :equilateral
+    elsif [x, y, z].uniq.length == 2
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
+
+  def no_size?(array)
+    array.any? { |num| num <= 0 }
+  end
+
+  def inequality_broken?(array)
+    array.max >= array.min + array.sort[-2]
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
