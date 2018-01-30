b2b5adff935fece6d6f328abfe853a859d2de579
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-30 14:23:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,8 @@
+require 'pry'
 class Triangle
   # write code here
   attr_accessor :side_1, :side_2, :side_3
-  attr_reader :equilateral, :isoscles, :scalene
+
 
   def initialize(side_1, side_2, side_3)
     @side_1 = side_1
@@ -10,24 +11,20 @@ class Triangle
   end
 
   def kind
-    if side_1 <= 0 || side_2 <= 0 || side_3 <= 0 || (side_2 + side_3 > side_1) || (side_1 + side_3 < side_2) || (side_1 + side_2 > side_3)
+      if ((side_1 <= 0) || (side_2 <= 0) || (side_3 <= 0) || (side_2 + side_3 <= side_1) || (side_1 + side_3 <= side_2) || (side_1 + side_2 <= side_3))
       begin
         raise TriangleError
-      rescue TriangleError => error
-        puts error.message
       end
-    elsif side_1 == side_2 && side_1 == side_3 && side_2 == side_3
+    elsif ((side_2 == side_3 && side_2 != side_1 && side_3 != side_1) || (side_1 == side_3 && side_1 != side_2 && side_2 != side_3) || (side_1 == side_2 && side_1 != side_3 && side_2 != side_3))
+      :isosceles
+    elsif ((side_1 == side_2 ) && (side_1 == side_3) && (side_2 == side_3))
        :equilateral
-     elsif side_2 == side_3 || side_1 == side_3 || side_1 == side_2
-       :isosceles
-     elsif side_1 != side_2 && side_1 != side_3 && side_2 != side_3
+    else
        :scalene
      end
   end
+end
 
   class TriangleError < StandardError
-    def message
-      "The sum of the lengths of any two sides of a triangle must exceed the length of the third side. Further, each side must be larger than 0."
-    end
+
   end
-end
