1e19f196c1ab41b10bb6a978978fdc6e53b71438
Barbara Choy <barbchoy@gmail.com>
2018-02-01 22:11:59 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,33 @@
 class Triangle
-  # triangle code
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    case
+    when side1 == 0 || side2 == 0 || side3 == 0
+        raise TriangleError
+    when side1 < 0 || side2 < 0 || side3 < 0
+        raise TriangleError
+    when (side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <=  side1
+        raise TriangleError
+    when side1 == side2 && side2 == side3
+      :equilateral
+    when side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
 
 class TriangleError < StandardError
- # triangle error code
+  def message
+      "You have a bad triangle!!"
+  end
 end
