8850efcd8e0bad882abb15024b16066a40272993
Jennifer Dobak <jdobak1@gmail.com>
2018-02-01 22:15:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 class Triangle
   # write code here
+attr_reader :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if validate_triangle == nil
+      raise TriangleError
+  elsif a == b && b == c
+    :equilateral
+  elsif a == b || b == c || c == a
+    :isosceles
+  else
+    :scalene
+  end
+  end
+
+  def validate_triangle
+    if a + b > c && b + c > a && a + c > b && a > 0 && b > 0 && c > 0
+      true
+    end
+  end
+
+end
+
+
+
+
+
+class TriangleError < StandardError
 end
