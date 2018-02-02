0017a2a9e6be8ec35e8da4c00ffe600b631f48dd
Amanda White <emmalinerosewhite@gmail.com>
2018-02-02 15:29:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require "pry"
 class Triangle
-  # write code here
+
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    if @one <= 0 || @two <= 0 || @three <= 0
+      raise TriangleError
+    elsif @one + @two <= @three || @two + @three <= @one || @one + @three <= @two
+      raise TriangleError
+    elsif @one == @two && @two == @three
+      return :equilateral
+    elsif @one == @two || @two == @three || @one == @three
+      return :isosceles
+    else
+      :scalene
+    end
+
+  end
+end
+
+
+
+class TriangleError < StandardError
+  def message
+    "your measurements are invalid."
+  end
 end
