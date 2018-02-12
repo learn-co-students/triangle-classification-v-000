ad16610f53e0500bd5cf478bc75e3cb2ee40cd66
github username <github email address>
2018-02-12 12:15:23 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :length_one, :length_two, :length_three
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+  end
+
+  def kind
+    if length_one <= 0 || length_two <= 0 || length_three <= 0 || length_one == length_two && length_three >= length_one + length_two || length_one == length_three && length_two >= length_one + length_three || length_two == length_three && length_one >= length_two + length_three ||  length_one >= length_two + length_three || length_two >= length_one + length_three || length_three >= length_two + length_three
+      raise TriangleError
+    elsif length_one == length_two && length_two != length_three || length_two == length_three && length_two != length_one || length_one == length_three && length_one != length_two && length_one > 0 && length_two > 0 && length_three > 0
+      :isosceles
+    elsif length_one != length_two && length_two != length_three && length_three != length_one && length_one > 0 && length_two > 0 && length_three > 0
+      :scalene
+    elsif length_one == length_two && length_two == length_three && length_one > 0 && length_two > 0 && length_three > 0
+    :equilateral
+    end
+  end
 end
+
+class TriangleError < StandardError
+end
+
+binding.pry
