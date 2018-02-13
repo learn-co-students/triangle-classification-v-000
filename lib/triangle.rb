d35567470f6c81d86232f94a564ced9c32d5b597
Chely Ho <adept_inept@yahoo.com>
2018-02-13 19:41:28 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  def initialize(a, b, c)
+    @sides = [a,b,c]
+  end
+
+  def kind
+    if valid?
+      case sides_equal
+      when 3
+        :equilateral
+      when 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def valid?
+    sides.all? do |i|
+      i > 0 &&
+      sides.reduce(:+) - i > i  # triangle inequality sum of any 2 sides > 3rd side
+    end
+  end
+
+  def sides_equal                      # count number of matching sides
+    frequency = Hash.new(0)
+    sides.each {|i| frequency[i] += 1}
+    frequency.values.sort[-1]         # returns biggest frequency
+  end
+end
+
+class TriangleError < StandardError
+
 end
