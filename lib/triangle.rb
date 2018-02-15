74ec574ea9d46086910c71f8f5fd70c34cd0acd7
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-02-15 18:35:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @triangle_sides = [@side1, @side2, @side3]
+  end
+
+  
+  def kind
+    a, b, c = [@side1, @side2, @side3].sort
+    if [@side1, @side2, @side3].min <= 0 || a + b <= c
+      begin
+        raise TriangleError
+      end
+    else
+      case [@side1, @side2, @side3].uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else        :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
 end
