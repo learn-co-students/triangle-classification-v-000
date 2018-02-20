e346960d04f2d780f3d8126a095f1a983d7a9f4a
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-02-20 15:37:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    raise TriangleError unless @side_1 > 0 and @side_1 + @side_2 > @side_3 and @side_2 + @side_3 > @side_1 and @side_1 + @side_3 > @side_2
+
+    if @side_1 == @side_2 && @side_2 == @side_3
+      :equilateral #always has equal sides
+
+      elsif @side_1 == @side_2 && @side_1 != @side_3 || @side_2 == @side_3 && @side_2 != @side_1 || @side_1 == @side_3 && @side_1 != @side_2
+      :isosceles #any two sides are equal but never three sides
+
+    elsif @side_1 != @side_2 && @side_2 != @side_3
+      :scalene #never has equal sides
+    end
+  end
+end
+
+
+
+
+class TriangleError < StandardError
+  # triangle error code
 end
