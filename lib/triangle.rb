d2b2cb5d8baf6dcf1201f42e2bc1542d0aec98e2
Mallory Feuer <mallory.feuer@gmail.com>
2018-02-26 22:00:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :x, :y, :z
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  def kind
+    if @x <= 0 || @y <= 0 || @z <= 0
+      raise TriangleError
+    elsif @x + @y <= @z || @x + @z <= @y || @z + @y <= @x
+      raise TriangleError
+    else
+      if @x == @y && @y == @z
+        :equilateral
+      elsif @x == @y || @y == @z || @x == @z
+        :isosceles
+      else
+        :scalene
+      end
+    end 
+  end
 end
+
+class TriangleError < StandardError
+  def message
+    "Each side must be larger than zero, and the sum of the lengths of any two sides always exceeds the length of the third side."
+  end
+end
+  
