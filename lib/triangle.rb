7db4199f816a2c7e5009b1bf29d2d5d5cfc8df7a
Zac Willmington <zac.willmington@gmail.com>
2018-02-27 21:59:16 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
+require "pry"
 class Triangle
+
   # write code here
-end
+
+  attr_accessor  :equilateral, :isosceles, :scalene, :kind, :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+      @side_one = side_one
+      @side_two = side_two
+      @side_three = side_three
+  end
+
+  def kind
+      if self.side_two <= 0 || self.side_one <= 0 || self.side_three <= 0
+          raise TriangleError
+
+      elsif self.side_two == self.side_one && self.side_three == self.side_one && self.side_two == self.side_three
+          #binding.pry
+           :equilateral
+
+      elsif self.side_two == self.side_one || self.side_three == self.side_one || self.side_two == self.side_three
+      #binding.pry
+         :isosceles
+
+    elsif  self.side_two != self.side_one && self.side_three != self.side_one && self.side_two != self.side_three
+         :scalene
+
+        end
+    end
+  end
+
+      class TriangleError < StandardError
+          def message
+              "you must give the get_married method an argument of an instance of the person class!"
+          end
+      end
