4fee4c2fa21c6c103c13a32809e5084aa34bf77a
Veronica Kim <vero.a.kim@gmail.com>
2018-03-06 02:13:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -29,8 +29,7 @@ class Triangle
       elsif (@side_one != @side_two && @side_three ==
         @side_two) || (@side_one == @side_two && @side_three != @side_two) || (@side_one == @side_three && @side_one != @side_two)
         return :isosceles
-      elsif @side_one != @side_two && @side_two !=
-        @side_three
+      else
         return :scalene
       end
 
