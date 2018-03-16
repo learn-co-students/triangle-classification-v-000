ee639fe71057ab756fe33c5973b3ca218f9333c6
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-16 00:39:08 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+  def initialize(a,b,c)
+    @side = [a, b, c].sort
+  end
+  
+  def kind
+    if @side[0] + @side[1] > @side[2]
+      if @side[0] == @side[2]
+        :equilateral
+      elsif @side[0] == @side[1] || @side[1] == @side[2]
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+end
+
+class TriangleError < StandardError
+  
 end
