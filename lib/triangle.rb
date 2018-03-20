8cfac41e755548b3ac8b214bf6909625464ed62e
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-20 05:10:00 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,9 +12,8 @@ class Triangle
   def kind
     validate
     return :equilateral if @shape.uniq.length == 1
-    result = :isosceles if @shape.uniq.length == 2
-    result = :scalene if @shape.uniq.length == 3
-    result
+    return :isosceles   if @shape.uniq.length == 2
+    return :scalene     if @shape.uniq.length == 3
   end
 end
 
