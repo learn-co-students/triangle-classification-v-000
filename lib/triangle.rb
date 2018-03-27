dad805c94973d567138ab29515481f7189ee0adc
smithwebtek <brad@smithwebtek.com>
2018-03-26 21:19:58 -0400

    Add various options

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,49 @@
-require 'pry'
 class Triangle
-  attr_accessor :s1, :s2, :s3, :kind
 
-  def initialize(s1,s2,s3)
-    @s1 = s1
-    @s2 = s2
-    @s3 = s3
-  end
+attr_accessor :s1, :s2, :s3, :kind
 
-  def kind
-		if s1*s2*s3 <= 0 || s1 >= (s2+s3) || s2 >= (s1+s3) || s3 >= (s1+s2)
-			
-      begin
-        raise TriangleError
-          puts error.message
-          end
-		
-			# begin
-			# 	raise TriangleError
-			# rescue TriangleError => error
-			# 		puts error.message
-			# end
- 
+	def initialize(s1, s2, s3)
+		@s1 = s1
+		@s2 = s2
+		@s3 = s3
+	end
+
+	def kind
+	if (s1 * s2 * s3) == 0 || (s1 + s2) <= s3 || (s2 +  s3) <= s1 || (s3 + s1) <= s2
+	# validate_triangle
+		begin
+			raise TriangleError
+				# puts error.message
+		end
+
+		# begin
+		# 	raise TriangleError
+		# rescue TriangleError => error
+		# 		puts error.message
+		# end
+
+		elsif s1 ==  s2 && s1 == s3
+			self.kind = :equilateral
 	
-		elsif s1 == s2 && s1 == s3
-      self.kind = :equilateral 
-    elsif s1 == s2 || s1 == s3 || s2 == s3
-      self.kind = :isosceles
-    else
-      self.kind = :scalene
-    end
-  end
+		elsif s1 == s2 || s1 == s3 || s2 == s3 
+			self.kind = :isosceles
+	
+		else
+			self.kind = :scalene
+		end
+	end
+
+	# def validate_triangle
+  #   real_triangle = [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+  #   [s1, s2, s3].each { |s| real_triangle << false if s <= 0 }
+		
+	# 	raise TriangleError if real_triangle.include?(false)
+  # end
 end
 
+
 class TriangleError < StandardError
-  def message
-    "wtf? that ain't no triangle! Try again Skippy."
-  end
+	def message
+		puts "not a triangle"
+	end
 end
---
+++ lib/triangle2.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Triangle
+  attr_accessor :s1, :s2, :s3, :kind
+
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+		if s1*s2*s3 <= 0 || s1 >= (s2+s3) || s2 >= (s1+s3) || s3 >= (s1+s2)
+			
+      begin
+        raise TriangleError
+          puts error.message
+          end
+		
+			# begin
+			# 	raise TriangleError
+			# rescue TriangleError => error
+			# 		puts error.message
+			# end
+ 
+	
+		elsif s1 == s2 && s1 == s3
+      self.kind = :equilateral 
+    elsif s1 == s2 || s1 == s3 || s2 == s3
+      self.kind = :isosceles
+    else
+      self.kind = :scalene
+    end
+  end
+end
+
+class TriangleError < StandardError
+  def message
+    "wtf? that ain't no triangle! Try again Skippy."
+  end
+end
