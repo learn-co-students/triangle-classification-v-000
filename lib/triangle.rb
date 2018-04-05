b77c649d2ba65c8a9f878fd347f1de82ec1f059f
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-05 20:07:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
-end
+
+  attr_accessor :side1, :side2, :side3, :equilateral,
+                 :isosceles, :scalene
+
+       def initialize(side1, side2, side3)
+         @side1 = side1
+         @side2 = side2
+         @side3 = side3
+       end
+
+       def kind
+             if side1 == 0 && side2 == 0 && side3 ==0
+               raise TriangleError
+             elsif side1 <= -1 || side2 <= -1 || side3 <= -1
+               raise TriangleError
+             elsif (side1 + side2 <= side3) || (side2 + side3 <= side1) || (side1 + side3 <= side2)
+               raise TriangleError
+
+             elsif side1 == side2 && side2 == side3
+               return :equilateral
+             elsif
+               side1 == side2 || side2 == side3 || side1 == side3
+               return :isosceles
+             elsif
+               side1 != side2 && side2 != side3
+               return :scalene
+             end #if statement
+       end #kind method
+ end #Triangle class
+
+ class TriangleError < StandardError
+   def notice
+     "The triangle measurements provided do not meet the conditions of
+     equilateral, isosceles or scalene triangles."
+   end
+ end
