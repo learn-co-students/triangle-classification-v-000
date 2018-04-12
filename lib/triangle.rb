017a78986f44803df9dac2cd7458c9b5c803e61d
EC2 Default User <ec2-user@ip-172-31-9-0.ec2.internal>
2018-04-12 21:20:59 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  # triangle code
+  attr_accessor :side_a, :side_b, :side_c, :kind
+  
+  def initialize(side_a, side_b, side_c)
+      @side_a = side_a
+      @side_b = side_b
+      @side_c = side_c
+  end  
+  
+  def kind
+      if @side_a <= 0 ||  @side_b <= 0 ||  @side_c <= 0 
+            begin
+                raise TriangleError
+                puts error.message
+            end 
+      end 
+     
+      if (@side_a + @side_b) <= @side_c || (@side_b + @side_c) <= @side_a || (@side_a + @side_c) <= @side_b
+          begin
+                raise TriangleError
+                puts error.message
+          end 
+      end  
+      
+      if @side_a == @side_b && @side_b == @side_c
+            self.kind = :equilateral
+      elsif @side_a == @side_b || @side_a == @side_c || @side_c == @side_b
+            self.kind = :isosceles
+      elsif @side_a != @side_b && @side_a != @side_c && @side_c != @side_b
+            self.kind = :scalene
+      end
+  end
+  
+  class TriangleError < StandardError
+      def message
+           "TriangleError"
+      end    
+  end
 end
