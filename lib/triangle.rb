fdc31bb41bd7528c6c7d3968ca6b8d1fbf3d1ae6
Isaac Villicana <villicana.i@gmail.com>
2018-04-19 21:52:16 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
+
+
 class Triangle
-  # write code here
+  class TriangleError < StandardError
+    def message
+      "Triangle is invalid with given measurements!"
+    end
+  end
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [@side1, @side2, @side3]
+  end
+
+  def equilateral?
+    return :equilateral if @side1 == @side2 && @side2 == @side3
+  end
+
+  def isosceles?
+    if (@side1 == @side2 && @side2 != @side3) ||
+      (@side1 == @side3 && @side3 != @side2) ||
+      (@side2 == @side3 && @side3 != @side1)
+      return :isosceles
+    end
+  end
+
+  def scalene?
+    return :scalene if @side1 != @side2 && @side2 != @side3
+  end
+
+  def valid?
+    if (@side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1) || @sides.any? {|x| x <= 0}
+      raise TriangleError
+    end
+  end
+
+  def kind
+    self.valid?
+    self.equilateral? || self.isosceles? || self.scalene?
+  end
 end
