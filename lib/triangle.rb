eb9c2b714e889631ad4958fd89640d4506f86adf
Brandon Chin <brandonchin@me.com>
2018-04-20 23:37:07 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -21,8 +21,8 @@ class Triangle
   end #def kind
 
   def validation
-    raise TriangleError if(side1 <= 0 || side2 <= 0 || side3 <= 0)
-    raise TriangleError if(side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
+    raise TriangleError if (side1 <= 0 || side2 <= 0 || side3 <= 0)
+    raise TriangleError if (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side1 + side2)
   end
 
   class TriangleError < StandardError
