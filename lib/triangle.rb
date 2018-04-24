3a3adc7006f3fe69b2f5362537bdb09edb9632ab
Nathan Dietz <nsdietz@gmail.com>
2018-04-24 02:47:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :sideone, :sidetwo, :sidethree
+
+  def initialize(sideone, sidetwo, sidethree)
+    @sideone = sideone
+    @sidetwo = sidetwo
+    @sidethree = sidethree
+  end
+
+  def kind # returns the type of triangle: equilateral, isosceles, scalene
+    raise TriangleError if sideone == 0 || sidethree < 0 || sideone + sidetwo < sidethree || sideone + sidethree <= sidetwo || sidetwo + sidethree < sideone
+    if sideone == sidetwo && sidetwo == sidethree && sideone == sidethree
+      :equilateral
+    elsif sideone == sidetwo || sidetwo == sidethree || sideone == sidethree
+      :isosceles
+    else sideone != sidetwo && sidetwo != sidethree && sideone != sidethree
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "violating the triangle inequality rule is illegal."
+    end
+  end
 end
