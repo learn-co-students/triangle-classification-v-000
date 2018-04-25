19821ca88afe7cfd6945d0eca5a49219ee8406a2
eyi1 <ej7729@gmail.com>
2018-04-24 22:07:02 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+
+  attr_accessor :x, :y, :z
+  
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z 
+  end
+
+  def kind   
+    if real_triangle_test1 == true && real_triangle_test2 == true 
+      true
+    else 
+      raise TriangleError   
+    end
+
+    if x == y && x == z && y == z 
+      :equilateral
+    elsif x != y && x != z && y != z
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def real_triangle_test1
+    x.positive? && y.positive? && z.positive? 
+  end
+
+  def real_triangle_test2
+    (x + y > z) && (x + z > y) && (y + z > x)
+  end
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
+
