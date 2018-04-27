5f9110fa51d9f8f742beb59d32f2fe11839dc8f5
Chrissa Constantine <chrissa1@mac.com>
2018-04-27 20:47:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+
+  attr_accessor :x, :y, :z
+
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+
+  end
+
+  def kind
+    if triangle == true
+    else
+      raise TriangleError
+    end
+    if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  def triangle
+    x.positive? && y.positive? && z.positive?
+    x + y > z && x + z > y && y + z > x
+  end
+
+  class TriangleError < StandardError
+
+    def message
+      "this is not a triangle!"
+    end
+
+  end
+
 end
