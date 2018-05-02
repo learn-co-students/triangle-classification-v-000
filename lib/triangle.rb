2f3ef4119c2785f1e90e82ddf4762bf3bfff1027
Marcus  = Soulo <souloinc87@gmail.com>
2018-05-02 18:59:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :side_a, :side_b, :side_c
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+  if (@side_a * @side_b * @side_c) == 0 || (@side_a + @side_b) <= @side_c || (@side_b + @side_c) <= @side_a || (@side_a + @side_c) <= @side_b 
+      begin
+        raise TriangleError
+          puts error.message
+      end
+    elsif @side_a == @side_b && @side_b == @side_c
+      :equilateral
+    elsif @side_a < @side_b && @side_b == @side_c || @side_a > @side_b && @side_a == @side_c ||
+      @side_a == @side_b && @side_b > @side_c
+      :isosceles
+    elsif @side_a != @side_b && @side_b != @side_c
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Each side must equal something!"
+    end
+  end
 end
