60d3cd393ee774629167d9b5a2cd789d2e3b5da3
Alejandro Sabogal <asabogal.git@gmail.com>
2018-05-03 00:36:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    inequality
+
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    elsif a != b && b != c
+      :scalene
+    end
+
+
+  end
+
+  def inequality
+    if (a <= 0 && b <= 0 && c <= 0) || (a + b <= c || b + c <= a || a + c <= b)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
