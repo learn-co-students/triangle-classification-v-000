4345d4c401a4c4b6b9757352f851b7ce1d4456f4
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-07 19:35:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+
+attr_accessor :one, :two, :three
+
+def initialize(one, two, three)
+  @one = one
+  @two = two
+  @three = three
+end
+
+def kind
+  if one == two && two == three
+    :equilateral
+  elsif one == two || two == three || one == three
+    :isosceles
+  else
+    :scalene
+  end
+end
+
 end
