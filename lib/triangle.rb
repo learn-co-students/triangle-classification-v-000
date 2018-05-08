3b2512155108cc631f472a3f493731904ed09fd4
ERIC KELLY <eakelly220@gmail.com>
2018-05-08 02:43:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  def kind 
+    if (@side1 == @side2 && @side1 == @side3) && @side1 != 0
+      :equilateral
+    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+      :isosceles
+    elsif (@side1 != @side2 && @side1 != @side3 && @side2 != @side3
+      :scalene
+    else 
+        begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    end 
+  end 
+  
+  class TriangleError < StnadardError 
+      def message 
+      "Invalid Side Length"
+    end
+  end 
 end
