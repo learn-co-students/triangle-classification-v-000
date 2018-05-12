bfa490e7a764fbd21374ab017de2bd5bc853b1d3
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-12 21:09:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+      @side1 = side1
+      @side2 = side2
+      @side3 = side3
+  end
+
+  def kind
+    sides = [side1, side2, side3].sort
+    if sides[0] <= 0 || (sides[0] + sides[1]) <= sides[2]
+      #binding.pry
+      raise TriangleError
+    elsif sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    else
+     :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
