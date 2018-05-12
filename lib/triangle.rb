4e23dc18d654aa3f118f2889d48814c53e6c7e5f
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-12 01:59:55 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,12 +12,11 @@ class Triangle
       if @side_one < 0 || @side_two < 0 || @side_three < 0
         raise TriangleError
       end
-      if @side_one + @side_two < @side_three || @side_three + @side_two < @side_one
+
+      if @side_one + @side_two < @side_three || @side_three + @side_two < @side_one || @side_one + @side_three <= @side_two
         raise TriangleError
       end
-      if @side_one + @side_three < @side_two
-        raise TriangleError
-      end
+
       if @side_one == @side_two && @side_two == @side_three && @side_three == @side_one
         if @side_one == 0
           raise TriangleError
@@ -28,9 +27,6 @@ class Triangle
         :scalene
       else @side_one != @side_two && @side_two != @side_three
         :isosceles
-      # else @side_one == 0 && @side_two == 0 && @side_three == 0 || @side_one + @side_two < @side_three || @side_three + @side_two < @side_one || @side_three + @side_one < @side_two
-      #   raise TriangleError
-      # rescue
     end
   end
 
