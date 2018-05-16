730173a96ced3621b60b457102d5598a7bd33709
Kitty Davies <kittyfdavies@gmail.com>
2018-05-16 18:23:30 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Triangle
   attr_accessor :side_1, :side_2, :side_3
 
@@ -9,13 +11,10 @@ class Triangle
 
   def kind
     if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-      begin
-        raise TriangleError
-      end
-    elsif (@side_1 + @side_2) < @side_3 || (@side_1 + @side_3) < @side_2 || (@side_2 + @side_3) < @side_1
-      begin
-        raise TriangleError
-      end
+      raise TriangleError
+    elsif (@side_1 + @side_2) <= @side_3 || (@side_1 + @side_3) <= @side_2 || (@side_2 + @side_3) <= @side_1
+      #binding.pry
+      raise TriangleError
     elsif @side_1 == @side_2 && @side_2 == @side_3
       :equilateral
     elsif (@side_1 == @side_2 && @side_2 != @side_3) || (@side_1 == @side_3 && @side_2 != @side_3) || (@side_2 == @side_3 && @side_1 != @side_3)
