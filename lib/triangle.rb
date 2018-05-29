3c1158090979240edb6ecf4c5dc6b3ac013ef8e2
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-29 23:54:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
+# no side lengths <= zero, sum of any two sides must exceed the third 
+
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    # types = :equilateral, :isosceles, :scalene
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
