445727e58ea924e69e7b3508aa4b61f35e056a1c
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-30 03:25:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,6 @@
-# no side lengths <= zero, sum of any two sides must exceed the third 
-
 class Triangle
   attr_accessor :side1, :side2, :side3
-  
+
   def initialize(side1, side2, side3)
     @side1 = side1
     @side2 = side2
@@ -10,45 +8,37 @@ class Triangle
   end
 
   def high_enough?
-   unless side1 <= 0 || side2 <= 0 || side3 <= 0
+    unless side1 <= 0 || side2 <= 0 || side3 <= 0
       true
     else
       false
     end
   end
-  
+
   def balanced_sides?
-    if side1 + side2 > side3 || side1 + side3 > side2 || side2 + side3 > side1
+    if side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
       true
     else
       false
     end
   end
-  
+
   def kind
-    if high_enough? && balanced_sides?
+    unless high_enough? && balanced_sides?
+      raise TriangleError
+    else
       if side1 == side2 && side1 == side3
-        # all sides equal
         :equilateral
       elsif side1 == side2 || side1 == side3 || side2 == side3
-        # two sides equal
         :isosceles
       else
-        # no equal sides
         :scalene
       end
-    else
-      begin
-        raise TriangleError
-      rescue TriangleError => error 
-        puts error.message
-      end
-      false
     end
   end
-  
+
   class TriangleError < StandardError
-    def message 
+    def message
       "Invalid triangle: remember that side lengths have to be higher than zero, and the sum of any two sides cannot exceed the third"
     end
   end
