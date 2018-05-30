6c86282d68259ecb28e8f2b14253657f79cce662
Carrie Savinell <carrielane89@gmail.com>
2018-05-30 15:37:44 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,39 @@
+require 'pry'
 class Triangle
   
-  attr_accessor :equilateral, :isosceles, :scalene
+  attr_reader :x, :y, :z
   
-  def initialize(equilateral, isosceles, scalene)
-    @equilateral = equilateral
-    @isosceles = isosceles
-    @scalene = scalene
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
   end   
   
   def kind 
-    
+    valid_triangle
+    if x == y && y == z
+      :equilateral
+    elsif x == y || y == z || x == z
+      :isosceles
+    else
+      :scalene 
+    end   
+  end  
+  
+  def valid_triangle
+    valid = true 
+    inequality_triangle = [(x + y > z), (y + z > x), (z + x > y)]
+   # valid = false if inequality_triangle.any? {|triangle| triangle != triangle  } 
+   
+    sides = [x, y, z]
+    sides.each do |side|
+       inequality_triangle << false if sides.any? {|side| side <= 0} 
+   
+   # valid = false if sides.any? {|side| side <= 0} 
+    end 
+    raise TriangleError if inequality_triangle.include?(false)
+   # original 
+   # raise TriangleError if valid == false
   end   
   
   class TriangleError < StandardError 
