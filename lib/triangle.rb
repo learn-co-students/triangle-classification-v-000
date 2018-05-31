0cd232c107e459fe3466050833a7d239e51a263b
Alex Zhang <alexgzhang92@gmail.com>
2018-05-31 20:47:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+  attr_accessor :first_side, :second_side, :third_side
+  
+  def initialize(first_side, second_side, third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side
+  end
+  
+  def triangle_inequality?
+    if @first_side + @second_side <= @third_side
+      true
+    elsif @second_side + @third_side <= @first_side
+      true
+    elsif @first_side + @third_side <= @second_side
+      true
+    else
+      false
+    end
+  end
+  
+  def kind
+    if @first_side <= 0 || @second_side <= 0 || @third_side <= 0
+      raise TriangleError
+    elsif triangle_inequality?
+      raise TriangleError
+    elsif @first_side == @second_side && @second_side == @third_side
+      :equilateral
+    elsif @first_side == @second_side && @second_side != @third_side
+      :isosceles
+    elsif @first_side != @second_side && @second_side == @third_side
+      :isosceles
+    elsif @first_side == @third_side && @first_side != @second_side
+      :isosceles
+    elsif @first_side != @second_side && @second_side != @third_side && @first_side != @third_side
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    "Your triangle is invalid. Please try again."
+  end
 end
+
+
