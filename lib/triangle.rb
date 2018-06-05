a105e6d584704f547539c734d233dbc874e627db
Heidi Hlavinka <hchlav@gmail.com>
2018-06-05 15:39:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    real_triangle
+    if a==b && b==c
+      :equilateral
+    elsif a==b || b==c || a==c
+      :isosceles
+    elsif a!=b && b!=c && a!=c
+      :scalene
+    end
+  end
+
+  def real_triangle
+    triangle = a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0
+    raise TriangleError if !triangle
+    # binding.pry
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This isn't a triangle."
+    end
+  end
+
 end
