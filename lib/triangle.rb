e1d243facdecb556d84184b790e92869949746b6
Dmitri Mendis <2dmendis@gmail.com>
2018-06-05 18:07:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :side_one, :side_two, :side_three, :equilateral, :isosceles, :scalene
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one 
+    @side_two = side_two 
+    @side_three = side_three
+  end 
+  
+  def kind 
+    if ((@side_one + @side_two > @side_three) && (@side_one + @side_three > @side_two) && (@side_two + @side_three > @side_one))
+      if @side_one == @side_two && @side_two == @side_three
+        triangle = :equilateral 
+      elsif ((@side_one == @side_two && @side_two != @side_three) || (@side_one == @side_three && @side_three != @side_two) ||(@side_three == @side_two && @side_two != @side_one))
+        triangle = :isosceles
+      else
+        triangle = :scalene
+      end 
+    else 
+      #begin 
+      raise TriangleError
+      #rescue TriangleError => error
+      #end 
+    end 
+  end 
+  
+  class TriangleError < StandardError
+  end 
 end
