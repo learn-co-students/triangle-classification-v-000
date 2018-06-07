343a1e3b6e15d03980a38e6b921edc69a51dea0d
Donnovan Lyons <dlyons8@binghamton.edu>
2018-06-07 14:51:57 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [@side1,@side2,@side3]
+  end
+
+  def kind
+    if invalid?
+        raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
+      return :equilateral
+    elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
+      return :isosceles
+    elsif @side1 != @side2 && @side2 != @side3
+      return :scalene
+    end
+  end
+
+  def invalid?
+    if @sides.any? {|side| side <= 0}
+      true
+    elsif @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      true
+    end
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
