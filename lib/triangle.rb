a5807d2eb6895166bda00cf5768ad48608b1db96
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 03:25:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,55 @@
 class Triangle
-  # write code here
+  
+  attr_reader :length_1, :length_2, :length_3, :equilateral, :isosceles, :scalene
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+  
+  def false_triangl?
+    if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
+        true
+      elsif @length_1 + @length_2 <= @length_3
+        true
+      elsif @length_2 + @length_3 <= @length_1
+        true
+      elsif @length_1 + @length_3 <= @length_2
+        true
+      else 
+        false 
+    end 
+  end
+
+  def valid_triangl?
+        if @length_1 == @length_2 && @length_2 == @length_3 
+      :equilateral
+    elsif @length_1 == @length_2 && @length_2 != @length_3 
+      :isosceles
+    elsif @length_1 != @length_2 && @length_2 == @length_3
+      :isosceles
+    elsif @length_1 == @length_3 && @length_1 != @length_2
+      :isosceles
+    else @length_1 != @length_2 && @length_2 != @length_3 && @length_1 != @length_3
+      :scalene
+    end
+  end 
+    
+  def kind
+    if false_triangl? == true 
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    else 
+      valid_triangl?
+    end
+      
+  end
+  
+  class TriangleError < StandardError
+    def message
+    puts "Not a valid triangle. Try again."
+  end
+  end
 end
