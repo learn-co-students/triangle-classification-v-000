2e18fe95643d851a1915038e54e1b9be43cd121c
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-06-14 00:11:02 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
-class Triangle
-  # write code here
+
+
+ class Triangle
+  attr_reader :side_a, :side_b, :side_c
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    validate_triangle
+    if side_a == side_b && side_b == side_c
+      :equilateral
+    elsif side_a == side_b || side_b == side_c || side_a == side_c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate_triangle
+    valid_triangle = [(side_a + side_b > side_c), (side_a + side_c > side_b), (side_b + side_c > side_a)]
+    [side_a, side_b, side_c].each { |s| valid_triangle << false if s <= 0 }
+    raise TriangleError if valid_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
