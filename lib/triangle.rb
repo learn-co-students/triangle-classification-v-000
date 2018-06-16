ec078c45a1df41281f04602b7948d1eb38a0e5f4
Eric Chrobak <eric@ericchrobak.com>
2018-06-16 19:28:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_accessor :side_one, :side_two, :side_three
+  def initialize(a, b, c)
+    @side_one = a
+    @side_two = b
+    @side_three = c
+  end
+
+  def kind
+    if @side_one <= 0 && @side_two <= 0 && @side_three <= 0
+      raise TriangleError
+    elsif @side_one < 0 || @side_two < 0 || @side_three < 0
+      raise TriangleError
+    elsif @side_one + @side_two <= @side_three || @side_one + @side_three <= @side_two || @side_two + @side_three <= @side_one
+      raise TriangleError
+    elsif @side_one == @side_two && @side_one == @side_three
+      type = :equilateral
+    elsif @side_one == @side_two || @side_one == @side_three || @side_two == @side_three
+      type = :isosceles
+    else
+      type = :scalene
+    end
+    type
+
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This violates the triangle inequality principle"
+    end
+  end
 end
