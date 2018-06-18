a4e21d951db35137a8e74f93c6acf53be4a3826a
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-18 18:50:16 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :length_1, :length_2, :length_3
+
+  def initialize(length_1, length_2, length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    invalid_triangle = @length_1 <= 0 || @length_2 <= 0 || @length_3 <= 0 || \
+      @length_1 + @length_2 <= @length_3 || @length_2 + @length_3 <= @length_1 || \
+      @length_1 + @length_3 <= @length_2
+
+    if invalid_triangle
+      raise TriangleError
+    elsif @length_1 == @length_2 && @length_2 == @length_3
+      :equilateral
+    elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_3 == @length_1
+      :isosceles
+    else
+      :scalene
+    end
+    
+  end
+
+  class TriangleError < StandardError
+    def message
+      "The given lengths are not a valid triangle."
+    end
+  end
 end
