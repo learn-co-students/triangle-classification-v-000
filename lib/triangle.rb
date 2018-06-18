ffb82e091bd764374163c9a7e5737770264182d0
Emily J Cain <emilycain@ymail.com>
2018-06-18 17:07:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :first_side, :second_side, :third_side
+
+  def initialize(first_side, second_side, third_side)
+    @first_side = first_side
+    @second_side = second_side
+    @third_side = third_side 
+  end 
+  
+  def kind 
+    
+    if (first_side <= 0) || (second_side <= 0) || (third_side <= 0)
+      raise TriangleError
+    elsif ((first_side + second_side) <= third_side) || ((first_side + third_side) <= second_side) || ((second_side + third_side) <= first_side)
+      raise TriangleError
+    else 
+      if (first_side == second_side) && (second_side == third_side) && (first_side == third_side) 
+          :equilateral 
+      elsif ((first_side == second_side) && (first_side != third_side)) || ((first_side == third_side) && (first_side != second_side)) || ((second_side == third_side) && (second_side != first_side))
+          :isosceles 
+      else (first_side != second_side) && (second_side != third_side) && (first_side != third_side)
+          :scalene 
+      end 
+    end 
+  end 
+  
+    class TriangleError < StandardError
+      
+    end 
+    
+
 end
