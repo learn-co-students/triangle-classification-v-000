4a2b567a18b9a316434644ddc56f817202ce4115
Megan Collier <meganraec@live.com>
2018-06-22 04:15:21 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,15 +19,23 @@ class Triangle
       raise TriangleError, "This shape does not meet the principle of triangle inequality."
     end
 
-    if a == b && b == c
-      then
-      :equilateral
-    elsif a == b || a == c || b == c
-      then
-      :isosceles
-    else
-      :scalene
+#Using a case is a very elegant solution I found to consolidate this code.
+    case [a,b,c].uniq.size
+    when 1 then :equilateral
+    when 2 then :isosceles
+    else        :scalene
     end
+
+#The following is my original code that performs the same task as the case above.
+    # if a == b && b == c
+    #   then
+    #   :equilateral
+    # elsif a == b || a == c || b == c
+    #   then
+    #   :isosceles
+    # else
+    #   :scalene
+    # end
   end
 end
 
