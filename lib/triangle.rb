572374782cdaf664a194aeecfa618e83b5a82222
Leticia Molina <letimolina@gmail.com>
2018-06-23 19:41:25 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,12 @@
 class Triangle
-  # write code here
+ attr_accessor :side_a, :side_b, :side_c
+ def initialize(side_a, side_b, side_c)
+   @side_a = side_a
+   @side_b = side_b
+   @side_c = side_c
+ end
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
