c5bc287ed6c22cc43cdc4100d6420258c8e5e313
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-24 16:29:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
+require "pry"
 class Triangle
-  # write code here
+  attr_accessor :s1,:s2,:s3
+
+  def initialize(s1,s2,s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+  end
+
+  def kind
+    valid?
+    if (s1 == s2 && s2 == s3)
+      :equilateral
+    elsif (s1 == s2 || s2 == s3 || s3 == s1)
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+
+def valid?
+  valid_triangle_check = [(s1 + s2 > s3),(s2 + s3 > s1),(s3 + s1 > s2)]
+    raise TriangleError if valid_triangle_check.include?(false) || s1 <= 0 || s2 <= 0 || s3 <= 0
+  end
+
+class TriangleError < StandardError
+end
+
 end
