8d02d199f3c1f8ad591bac6a275598297193bd47
Mohammed Noori <kmanoorib@gmail.com>
2018-07-03 01:40:11 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_accessor :one, :two, :three
+  
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end
+  
+  def kind
+    checkpoint
+    
+    if (one == two && two == three)
+      :equilateral
+    elsif (one == two || one == three || two == three)
+      :isosceles
+    else
+      :scalene
+    end
+    
+  end
+  
+  def checkpoint
+    is_triangle = true
+    [one, two, three].each {|number| is_triangle = false if number <= 0}
+    is_triangle = false if !(one + two > three) || !(one + three > two) || !(two + three > one) 
+    raise TriangleError if is_triangle == false
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
