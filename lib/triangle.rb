ac56579cea2a5f582547d3ee8311a3ce0e99f9ea
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-04 02:26:26 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_reader :side_1, :side_2, :side_3, :type
+
+  @@all = []
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @@all << self
+  end
+
+  def kind
+    if side_1 + side_2 <= side_3 || side_1 + side_3 <= side_2 || side_2 + side_3 <= side_1 || side_1 <= 0 || side_2 <= 0 || side_3 <= 0
+        raise TriangleError
+    else
+      if side_1 != side_2 && side_2 != side_3 && side_3 != side_1
+        @type = :scalene
+      elsif side_1 == side_2 && side_2 == side_3
+        @type = :equilateral
+      else
+        @type = :isosceles
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This triangle is not a triangle!!! Check the side lengths and try again"
+    end
+  end
+
 end
