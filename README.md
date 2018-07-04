b8ff207b16e493012de871420b55dce7a01f392c
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-04 13:42:33 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Triangle
+h # Triangle
 
 ## Objectives
 
--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
-end
+  
+  attr_accessor = :equilateral, :isosceles, :scalene
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end 
+  
+  def valid?
+    if @length1 <=0 || @length2 <=0 || @length3 <=0
+      false 
+    elsif @length1+@length2 <= @length3 ||@length2+@length3 <= @length1 || @length1+@length3 <= @length2
+      false
+    else
+      true
+    end 
+  end   
+  
+    def kind
+      
+      if self.valid?
+        if @length1 == @length2 && @length2 == @length3
+            :equilateral
+        elsif @length1 == @length2 || @length1 == @length3 || @length2 == @length3 
+            :isosceles
+        elsif @length1 > 0 && @length2 > 0 && @length3 > 0 
+            :scalene
+        end 
+      else 
+        begin
+          raise TriangleError
+        end 
+      end 
+    end
+   
+
+    class TriangleError < StandardError
+      def message
+        "Triangle Error"
+      end 
+    end 
+end 
