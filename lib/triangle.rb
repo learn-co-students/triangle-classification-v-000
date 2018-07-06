ec899e5ff5852db7adb8d4961aa86103a83e10cf
paulheisele@gmail.com <paulheisele@gmail.com>
2018-07-06 02:04:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  class TriangleError < StandardError
+    def error_message
+      "Sides lengths do not equate to a triangle "
+  end
+end
+
+  def kind
+    if (@side_1 || @side_2 || @side_3) <=0
+      raise TriangleError
+    elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 +@side_3 <=@side_1
+      raise TriangleError
+    elsif @side_1 == @side_2 && @side_2 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
 end
