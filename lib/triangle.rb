a3c6fcceb736cd615b54d8aadd8bf1fc223fb860
Brittany Tinnin <bntinnin7@gmail.com>
2018-07-11 12:27:50 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,19 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (@a == @b && @a == @c) && (@b == @a && @b == @c) && (@c == @a && @c == @b)
+      :equilateral
+    elsif (@a != @b) && (@b == @c) || (@a == @c) && (@c != @b) || (@a == @b) && (@b != @c)
+      :isosceles
+    elsif (@a != @b) && (@b != @c)
+      :scalene
+    end
+  end
 end
