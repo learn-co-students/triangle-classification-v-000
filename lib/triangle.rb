644ef9391ba77bff662846e73978abba219bb3b7
Jamie Brown <jmbrowncow@gmail.com>
2018-07-11 23:25:41 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,6 +22,23 @@ class Triangle
     true if @side_3 >= @side_1 + @side_2 || @side_1 >= @side_2 + @side_3 || @side_2>= @side_1 + @side_3  || @side_1 == 0 || @side_2 == 0 || @side_3 == 0
   end
 
+  # def kind
+  #   validate_triangle
+  #   if a == b && b == c
+  #     :equilateral
+  #   elsif a == b || b == c || a == c
+  #     :isosceles
+  #   else
+  #     :scalene
+  #   end
+  # end
+  #
+  # def validate_triangle
+  #   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+  #   [a, b, c].each { |s| real_triangle << false if s <= 0 }
+  #   raise TriangleError if real_triangle.include?(false)
+  # end
+
   class TriangleError < StandardError
     # triangle error code
   end
