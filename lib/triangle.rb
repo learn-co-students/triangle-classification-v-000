de3a26f8dafc12f9dc0eb3338fcfce5cce991752
Priscilla H <priscillamhuynh@gmail.com>
2018-07-11 11:26:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,9 @@
 class Triangle
-  # write code here
+  attr_accessor :side_one,:side_two,:side_three
+  
+  def initialize(side_one, side_two, side_three)
+    @side_one=side_one
+    @side_two=side_two
+    @side_three=side_three
+  end
 end
