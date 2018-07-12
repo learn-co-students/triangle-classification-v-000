5b69c55ab7303f65ee77c046160b065b9f8493c3
Valerie Moore <periwinkle17@outlook.com>
2018-07-12 23:51:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    valid
+    if (length1 == length2) && (length2 == length3)
+      :equilateral
+    elsif (length1 != length2) && (length2 == length3)
+      :isosceles
+    elsif (length2 != length3) && (length1 == length3)
+      :isosceles
+    elsif (length1 != length3) && (length1 == length2)
+      :isosceles
+    elsif (length1 != length2) && (length2 != length3) && (length1 != length3)
+      :scalene
+    end
+  end
+  
+    def valid
+      valid = [(length1 + length2 > length3), (length2 + length3 > length1), (length1 + length3 > length2)]
+      [length1, length2, length3].each { |length| valid << false if length <= 0 }
+      raise TriangleError if valid.include?(false)
+    end
+
+class TriangleError < StandardError
+    def self.message
+    "Not a valid triangle!"
+    end
+  end
+
 end
