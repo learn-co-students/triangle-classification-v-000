60f705bf62d7354f6b3561621805a2af61eef9b7
Amit Singh <amit_nyc@live.com>
2018-07-17 00:32:36 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+
+
+attr_accessor :side_1, :side_2, :side_3
+
+
+  def  initialize(side_1:, side_2:, side_3:)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind(symbol)
+    symbol =
+
+  end
 end
