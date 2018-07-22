87c7c26ef4baf4c6d608be4a24c0af245acdbfe0
Alicia Yu <alicia.yu.16@gmail.com>
2018-07-22 18:40:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate
+    if a == b && b == c
+    :equilateral
+    elsif a == b || b ==c || a==c
+    :isosceles
+    else
+    :scalene
+    end
+  end
+
+  def validate
+    sides = a + b > c && a + c > b && b + c > a
+    vaild = [a, b, c].all?(&:positive?) && sides
+    raise TriangleError unless vaild
+    end
+end
+
+class TriangleError < StandardError
 end
