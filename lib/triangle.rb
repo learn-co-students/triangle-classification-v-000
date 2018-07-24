557be64293e1c6b6a6fa38fa1edc6d59e57e337c
Adam Barton <adam.barton333@gmail.com>
2018-07-24 02:47:46 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,31 @@
 require 'pry'
 class Triangle
 
-  attr_accessor :side_1, :side_2, :side_3
+  attr_accessor :a, :b, :c
 
-  def initialize(side_1, side_2, side_3)
-    @side_1 = side_1
-    @side_2 = side_2
-    @side_3 = side_3
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if @side_1 == @side_2 && @side_2 == @side_3
+    valid_triangle?
+    if a == b && b == c
         :equilateral
-    elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+    elsif a == b || b == c || a == c
         :isosceles
-    elsif @side_1 != @side_2 && @side_2 != @side_3 && @side_1 != @side_3
+    else
         :scalene
-      elsif Triangle.kind != :scalene || :isosceles || :equilateral
-        begin
-          raise TriangleError
-        rescue TriangleError => error
-            puts error.message
-          end
     end
   end
 
-    class TriangleError < StandardError
-      def message
-      "That is not a triangle."
-      end
-    end
+  def valid_triangle?
+      real_triangle = [(a + b > c), (b + c > a), (a + c > b)]
+      [a, b, c].each { |x| real_triangle << false if x <= 0 }
+        raise TriangleError if real_triangle.include?(false)
+  end
 
+  class TriangleError < StandardError
+  end
 end
