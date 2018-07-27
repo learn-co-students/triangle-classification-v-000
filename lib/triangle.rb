68a23ef4b7e6f793e7eaa4707a42e8e0a420fded
Reno Sorensen <renojames19@gmail.com>
2018-07-27 01:35:17 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,7 +19,7 @@ class Triangle
   end
 
   def triangle?
-    (@side1 > 0 && @side2 > 0 && @side3 > 0) && (((@side1 + @side2) > @side3) || ((@side1 + @side3) > @side2) || ((@side2 + @side2) > @side1)))
+    (@side1 > 0 && @side2 > 0 && @side3 > 0) && (((@side1 + @side2) > @side3) || ((@side1 + @side3) > @side2) || ((@side2 + @side3) > @side1)))
   end
 
   def kind
