ede892f16e3c97934e4a806e815e3892d9f1e08e
Reno Sorensen <renojames19@gmail.com>
2018-07-27 01:26:05 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
     @side3 = side3
   end
 
-  def kind
+  def type
     if @side1 == @side2 && @side1 == @side3
       :equilateral
     elsif (@side1 == @side2 && @side1 != @side3) || (@side1 == @side3 && @side1 != @side2) || (@side2 == @side3 && @side1 != @side2)
@@ -16,6 +16,20 @@ class Triangle
     elsif @side1 != @side2 && @side1 != @side3 && @side2 !=@side3
       :scalene
     end
+  end
+
+  def triangle?
+    (@side1 > 0 && @side2 > 0 && @side3 > 0) && (((@side1 + @side2) > @side3) || ((@side1 + @side3) > @side2) || ((@side2 + @side2) > @side1)))
+  end
+
+  def kind
+
+    type
+  end
+
+
+  class TriangleError < StandardError
+
   end
 
 end
