e85b11749be72c93e81bb7d9ac18426696b79066
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-03 12:07:03 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,47 +1,61 @@
-class Triangle
+# class Triangle
   
 
-  attr_reader :a, :b, :c
+#   attr_reader :a, :b, :c
+#   def initialize(a, b, c)
+#     @a = a
+#     @b = b
+#     @c = c
+#   end
+  
+
+#   def kind
+#     validate_triangle
+#     if a == b && b == c
+#       :equilateral
+#     elsif a == b || b == c || a == c
+#       :isosceles
+#     else
+#       :scalene
+#     end
+#   end
+  
+#   def validate_triangle
+#   sides = [a,b,c]
+#   false if sides.any? { |side| side <= 0 }
+#   true if sides.all? { |side| side == a }
+#   sides.inject(:+) - sides.max > sides.max ? true : false
+#   raise TriangleError if sides.include?(false)
+# end
+
+#   # def validate_triangle
+#   #   real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+#   #   [a, b, c].each { |s| real_triangle << false if s <= 0 }
+#   #   raise TriangleError if real_triangle.include?(false)
+#   # end
+
+  
+#     class TriangleError < StandardError
+# end
+
+# end
+
+class Triangle
+  class TriangleError < StandardError
+end
+attr_reader :a, :b, :c
   def initialize(a, b, c)
     @a = a
     @b = b
     @c = c
   end
-  
-  def 
+
+def kind
+  sides = [a,b,c].sort
+
+  raise TriangleError if sides.first <= 0 || sides[2] >= sides[1] + sides[0]
+  return :equilateral if sides.uniq.length  == 1
+  return :isosceles if sides.uniq.length  == 2
+  :scalene
 end
-
-  def kind
-    valid_triangle?
-    if a == b && b == c
-      :equilateral
-    elsif a == b || b == c || a == c
-      :isosceles
-    else
-      :scalene
-    end
-  end
-  
-  def valid_triangle?
-  sides = [a,b,c]
-  false if sides.any? { |side| side <= 0 }
-  true if sides.all? { |side| side == a }
-  sides.inject(:+) - sides.max > sides.max ? true : false
-  raise TriangleError if false
-end
-
-#   def validate_triangle
-#     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-#     [a, b, c].each { |s| real_triangle << false if s <= 0 }
-#     raise TriangleError if real_triangle.include?(false)
-#   end
-
-# def valid_triangle?(a, b, c)
-#   a, b, c = [a, b, c].sort
-#   raise TriangleError if a <= 0 or a + b <= c
-# end
-  
-    class TriangleError < StandardError
-end
-
 end
