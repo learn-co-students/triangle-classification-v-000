e99d90bc47ff38c6b108abdf0590273aa8ca9d73
Tom  Cho <tc.thomascho@gmail.com>
2018-08-13 17:42:29 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three
+  end
+
+  def kind
+    #if !(side_one + side_two > side_three && side_two + side_three > side_one && side_one + side_three > side_two)
+      #raise TriangleError
+    #end
+
+    real_triangle = [(side_one + side_two > side_three), (side_one + side_three > side_two), (side_two + side_three > side_one)]
+    raise TriangleError if real_triangle.include?(false)
+
+
+    if side_one == side_two && side_two == side_three
+      :equilateral
+    elsif side_one == side_two || side_two == side_three || side_one == side_three
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
