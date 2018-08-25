5240bc4682bfdead04899ecf18d10bfdc1e6afda
Austin Powell <austinpowell58@gmail.com>
2018-08-25 16:17:57 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  require "pry"
+  attr_accessor :firstside, :secondside, :thirdside, :sidesarray
+  
+  class TriangleError < StandardError
+
+  end 
+  
+  
+  def initialize(firstside, secondside, thirdside)
+    @firstside = firstside
+    @secondside = secondside
+    @thirdside = thirdside
+    @sidesarray = [firstside.to_f, secondside.to_f, thirdside.to_f].sort
+  end 
+  
+  def kind
+    if (sidesarray[0] + sidesarray[1]) <= sidesarray[2]
+      raise TriangleError
+      
+    elsif @sidesarray.uniq.size == 1
+      :equilateral
+    elsif sidesarray[1] == sidesarray[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end 
+  
 end
