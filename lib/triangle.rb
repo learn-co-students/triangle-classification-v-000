995a77b3a9e02f7e19eb03b2bc39b92b372ce004
github username <github email address>
2018-08-28 22:26:31 -0400

    Initial commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,5 @@
-require 'pry'
-
 class Triangle
-  attr_accessor :s1, :s2, :s3, :equilateral, :isosceles, :scalene
+  attr_accessor :s1, :s2, :s3
 
   def initialize(s1, s2, s3)
     @s1 = s1
@@ -9,10 +7,14 @@ class Triangle
     @s3 = s3
   end
 
-  class TriangleError < StandardError
+  def valid?
+    unless (s1 > 0 && s2 > 0 && s3 > 0) && (s1 + s2 > s3 && s1 + s3 > s2 && s2 + s3 > s1)
+      raise TriangleError
+    end
   end
 
   def kind
+    valid?
     if s1 == s2 && s1 == s3
       :equilateral
     elsif s1 == s2 || s1 == s3 || s2 == s3
@@ -24,6 +26,7 @@ class Triangle
     end
   end
 
-end
+  class TriangleError < StandardError
+  end
 
-# Stop pry
+end
