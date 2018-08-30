1f2d67d6b7f28ef842b5df0aaaf7e3b6b93dc66b
Kari O'Neal <triadfirewood@gmail.com>
2018-08-30 11:38:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,40 @@
-class Triangle
-
-  def initialize(side_1, side_2, side_3)
-     @triangle_sides = []
-     @triangle_sides << side_1
-     @triangle_sides << side_2
-     @triangle_sides << side_3
-   
-  end
-
+class Triangle 
+  attr_accessor :side_1, :side_2, :side_3
+  
+  def initialize(s1, s2, s3)
+    @triangle_side = []
+    @triangle_side << s1
+    @triangle_side << s2
+    @triangle_side << s3
+  end  
+  
   def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-      if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0]) 
-        return TRUE
-      else
-        return FALSE
-      end
+    sum_s1_s2 = @triangle_side[0] + @triangle_side[1]
+    sum_s1_s3 = @triangle_side[0] + @triangle_side[2]
+    sum_s2_s3 = @triangle_side[1] + @triangle_side[2]
+    if (@triangle_side.none? {|side| side <= 0}) && (sum_s1_s2 >
+      @triangle_side[2] && sum_s1_s3 > @triangle_side[1] &&
+      sum_s2_s3 > @triangle_side[0])
+      return TRUE
+    else
+      return FALSE
     end
-
-  def kind 
-    if valid? 
-      if @triangle_sides.uniq.length == 1 
-        return :equilateral 
-      elsif @triangle_sides.uniq.length == 2 
-        return :isosceles 
-      else 
-        return :scalene 
-      end 
-    else 
-      raise TriangleError 
-    end 
+  end
+    
+  
+  def kind
+    if valid?
+      if @triangle_side.uniq.length == 1
+       :equilateral
+      elsif @triangle_side.uniq.length == 2
+        :isosceles
+      else
+       :scalene
+    end
+  else 
+    raise TriangleError
   end
 end
-
-class TriangleError < StandardError 
 end
-
+ class TriangleError<StandardError 
+end
