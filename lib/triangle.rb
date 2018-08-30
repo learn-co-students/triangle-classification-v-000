b17cabbadbf79f2e0e74ac3e1b99e421e3d7d558
Jenny K <jkam17@gmail.com>
2018-08-30 00:05:45 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :sides
+  
+  def initialize(s1,s2,s3)
+    @sides=[]
+    @sides << s1
+    @sides << s2
+    @sides << s3
+    @sides.sort!
+  end
+  
+  def sides
+    @sides 
+  end 
+  
+  def kind
+    if (self.sides[0]==0) || (self.sides[0]+self.sides[1] <= self.sides[2])
+      raise TriangleError
+    elsif self.sides[0]==self.sides[1] && self.sides[1]==self.sides[2]
+      :equilateral
+    elsif  self.sides[0]==self.sides[1] || self.sides[1]==self.sides[2]||self.sides[0]==self.sides[2]
+      :isosceles
+    else 
+      :scalene
+    end 
+  end 
+  
+  class TriangleError < StandardError
+  end
+  #binding pry
 end
