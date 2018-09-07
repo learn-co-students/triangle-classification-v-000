1b6e60877142ac65d96ca3149e1fa3098620e2c0
Michael Pieschel <sixtyhectares@yahoo.com>
2018-09-07 23:37:50 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,27 @@ class Triangle
     @c = c
   end
   
+  def is_triangle?
+    if !([a,b,c].max < [a,b,c].inject(:+)/2.0)
+        raise TriangleError
+    end
+  end
+  
   def kind
-  
-  
+    is_triangle?
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
   end
   
   class TriangleError < StandardError
+      "That is not a triangle!"
   end
+
 end
+
+
