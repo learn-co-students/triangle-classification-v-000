45f01300d6dd48ab185a3973443adc34fad4cbfa
Laura Darlak <laura@polyporedesign.com>
2018-09-07 19:40:52 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind
+    if (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
+      raise TriangleError
+    elsif (@side1 + @side2 <= @side3) || (@side1 + @side3 <= @side2) || (@side2 + @side3 <= @side1)
+      raise TriangleError
+    else
+      if @side1 == @side2 && @side2 == @side3
+        :equilateral
+      elsif (@side1 == @side2) || (@side2 == @side3) || (@side1 == @side3)
+        :isosceles
+      else (@side1 != @side2 || @side3) && @side2 != @side3
+        :scalene
+      end
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+      "A triangle must have three sides > 0"
+    end
+  end
 end
