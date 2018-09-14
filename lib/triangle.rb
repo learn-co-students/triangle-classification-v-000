fd95be023cb78f8c2ed90b2b94b1a30e4c8866da
Harley Rayner <harleymteden@gmail.com>
2018-09-14 19:17:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
-  # write code here
+
+  def initialize(length1, length2, length3)
+    Triangle.new(length1, length2, length3)
+    @length1 = length1
+    @lenght2 = length2
+    @length3 = length3
+  end
+
+  def valid?
+    @length1 + @length2 > @length3
+    @length1 + @length3 > @length2
+    @length2 + @length3 > @length1
+  end
+
+  def kind
+    if self.valid? != true
+      begin
+       raise TriangleError
+      rescue TriangleError => error
+       puts error.message
+      else
+        "no issues with this triangle"
+    end
+  end
+
+  class TriangleError < StandardError
+
+    def message
+     "this is not a valid triangle"
+    end
+
+  end
+
+
+
 end
