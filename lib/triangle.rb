43f3e930ce41324adf48c3e1ebe4062c946c364e
Harley Rayner <harleymteden@gmail.com>
2018-09-14 20:27:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,43 +1,52 @@
 require 'pry'
 
-class TriangleError < StandardError
-
-  def message
-   "this is not a valid triangle"
-  end
-
-end
-
 class Triangle
 
+  class TriangleError < StandardError
+
+    def message
+     "this is not a valid triangle"
+    end
+
+  end
+
   def initialize(length1, length2, length3)
-    # Triangle.new(length1, length2, length3)
     @length1 = length1
     @length2 = length2
     @length3 = length3
   end
 
   def valid?
-    @length1 + @length2 > @length3
-    @length1 + @length3 > @length2
-    @length2 + @length3 > @length1
-    @length1 == 0 || @length2 == 0 || @length3 == 0
+    @length1 + @length2 > @length3 && @length1 + @length3 > @length2 && @length2 + @length3 > @length1 && @length1 > 0 && @length2 > 0 && @length3 > 0
   end
 
   def kind
-
-    if @length1 == @length2 && @length1 == @length3
-      :equilateral
-    elsif
-      @length1 == @length2 && @length1 != @length3 || @length2 == @length3 && @length2 != @length1 || @length3 == @length1 && @length3 != @length2
-      :isosceles
-    elsif
-      @length1 != @length3 && @length3 != @length2
-      :scalene
-    # elsif self.valid? != true
-    #   TriangleError
-    end
-
+    if self.valid?
+        if @length1 == @length2 && @length1 == @length3
+          :equilateral
+        elsif
+          @length1 == @length2 && @length1 != @length3 || @length2 == @length3 && @length2 != @length1 || @length3 == @length1 && @length3 != @length2
+          :isosceles
+        elsif
+          @length1 != @length3 && @length3 != @length2
+          :scalene
+        end
+    else
+        begin
+          raise TriangleError
+        end
+      end
   end
+      #raise error
+
+
+
+
+  #     @length1 + @length2 > @length3 || @length1 + @length3 > @length2 || @length2 + @length3 > @length1 || @length1 == 0 || @length2 == 0 || @length3 == 0
+  #     begin
+  #
+  #   end
+  #
+  # end
 
 end
