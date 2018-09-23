5b8961507c5519c2602dcadf2095fd94b85fbcc5
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-23 21:51:24 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3, :sides
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+    @sides = [a, b, c]
+  end
+
+  def kind
+    if sides.none? { |side| side > 0 }
+      raise TriangleError
+    elsif (@a + @b <= @c) || (@a + @c <= @b) || (@b + @c <= @a)
+      raise TriangleError
+    end
+
+    case sides.uniq.size
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
