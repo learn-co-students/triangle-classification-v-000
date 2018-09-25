19f8df9090b1167a3365746a175b39279d36058a
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-09-25 00:09:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+ attr_reader :arg1, :arg2, :arg3
+ 
+ def initialize(arg1, arg2, arg3)
+   @arg1 = arg1
+   @arg2 = arg2
+   @arg3 = arg3
+ end
+ 
+  def kind
+    true_triangle
+    if arg1 == arg2 && arg2 == arg3
+      :equilateral
+    elsif arg1 == arg2 || arg2 == arg3 || arg1 == arg3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
+  
+  def true_triangle
+    real_triangle = [(arg1 + arg2 > arg3), (arg1 + arg3 > arg2), (arg2 + arg3 > arg1)]
+    [arg1, arg2, arg3].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
 end
