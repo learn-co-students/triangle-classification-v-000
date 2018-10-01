34cb6752ca20e0405e051af95cdc993747a2f2e2
Sherry Martin <sm674@cornell.edu>
2018-10-01 01:16:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+attr_accessor :x, :y, :z 
+
+#   It returns:
+#   :equilateral  if all sides are equal
+#   :isosceles    if exactly 2 sides are equal
+#   :scalene      if no sides are equal
+
+def initialize(x, y, z)
+  @x = x 
+  @y = y 
+  @z = z
+end 
+
+def kind
+    if not_negative == true && illegal == true 
+      puts true
+    else 
+      raise TriangleError 
+    end 
+     if x == y && y == z && x == z
+      :equilateral
+    elsif x != y && y != z && z != x
+      :scalene 
+    else 
+      :isosceles
+    end 
+  end
+
+  def not_negative 
+    x.positive? && y.positive? && z.positive?
+  end 
+  
+  def illegal
+    x + y > z && x + z > y && y + z > x
+    end 
+  end
+  
+  class TriangleError < StandardError
 end
