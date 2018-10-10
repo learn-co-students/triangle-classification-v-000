71c45a7fc02d299d145d27f56583c5456b69625c
Cheryl K <cherylkelson@icloud.com>
2018-10-10 20:01:54 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,43 +1,59 @@
 class Triangle
-attr_reader :equilateral, :isosceles, :scalene 
+attr_reader :a, :b, :c
 
-
-  def initialize(equilateral, isosceles, scalene)
-  #@length = length
-  @equilateral = equilateral 
-  @isosceles = isosceles
-  @scalene = scalene 
+  def initialize(length_a, length_b, length_c)
+  @a = length_a
+  @b = length_b
+  @c = length_c
   end 
 
+  def equilateral
+    a == b &&  b == c &&  a == c 
+  end 
+
+  def isosceles
+    a == b && b == c && c !=  a
+  end 
+
+  def scalene
+    a != b && b != c && c != a
+  end 
+ 
   def kind 
-    valid_triangle?
-    if a == b && b == c 
-      :equilateral 
-    elsif a == b || b == c || a == c 
-      :isosceles  
+    if a == b && b == c && a == c
+      :equilateral
+     
+    elsif a == b || b == c || c == a
+      :isosceles
+      
     else 
       :scalene
       end
+    # else
+    #   raise TriangleError
+    # end 
   end 
+
   
 
-  def definition_of_triangle
-    actual_triangle = [(a + b > c), (a + c > b), (b + c > a)] 
-    [a,b,c].each { |x| actual_triangle}
-      if [a,b,c] > 0 && actual_triangle
-            begin 
-        true
-            raise TriangleError
-      else
-        false
-            rescue TriangleError
-      end 
-    end 
+  def validate_triangle
+    true_triangle = [(a + b > c),  (a + c > b), (b + c > a)] 
+    
+    [a,b,c].each { |x| true_triangle << false if  x <= 0}
+      #if [a,b,c] > 0 && actual_triangle
+      #           true
+      # else    
+      
+      raise TriangleError if true_triangle.include? (false)
+      
+    end       
+            
+# #TriangleError => error
+ 
 
+class TriangleError < StandardError
+end 
 
-        class TriangleError < StandardError
-        end 
+end
 
-end      
-
-
+     
