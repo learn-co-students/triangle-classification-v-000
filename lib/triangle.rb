3bcb57a7730a847ff3e79d24576a8e0366b5a0ca
Susan Meyers <smmeyers24@gmail.com>
2018-10-19 21:07:23 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,17 +4,49 @@ class Triangle
   attr_accessor :side1, :side2, :side3
   
   def initialize(side1, side2, side3)
-    
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
   
   def kind
+    @side1 == @side2 && @side2 == @side3 
+      :equilateral
+      
+    @side1 == @side2 && @side1 != @side2 && @side1 != @side3
+       :isosceles
+   end
+ end
+
+     
+   
+    
+    
+    
+    
+     
+   
+  
+  
+  
+    
+    
+  
+
+
+# def is_valid_triangle?(one,two,three)
+#   sides = [one,two,three].sort
+#   if (sides[0] + sides[1] <= sides[2]) || (sides[0] == 0)
+#     return "invalid!"
+#   else
+#     which_triangle?(one,two,three)
+#   end
+# end
       
       
-    #Equilateral triangle have equal sides
+#     #Equilateral triangle have equal sides
     
   
     
     
-  end
-end
-
+ 
