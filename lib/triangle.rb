3b077df8b956c7e05f2368cb80fd5be92d4f1884
Susan Meyers <smmeyers24@gmail.com>
2018-10-23 21:12:48 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,25 +3,27 @@ class Triangle
   
   attr_accessor :side1, :side2, :side3
   
-  def initialize(side1, side2, side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
   
   def kind
-    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || (@side1 + @side2) >= @side3 || (@side2 + @side3) >= @side1 || (@side3 + @side1) >= @side2
+    
+    if s1*s2*s3 <= 0 || s1 >= (s2+s3) || s2 >= (s1+s3) || s3 >= (s1+s2) 
+    #if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || (@side1 + @side2) >= @side3 || (@side2 + @side3) >= @side1 || (@side3 + @side1) >= @side2
       
       begin
         raise TriangleError 
            puts error.message
         end
-     end
+    
       
-    if @side1 == @side2 && @side2 == @side3 
+    elsif @s1 == @s2 && @s2 == @s3 
       :equilateral
       
-    elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+    elsif @s1 == @s2 || @s1 == @s3 || @s2 == @s3
       :isosceles
         
     else
