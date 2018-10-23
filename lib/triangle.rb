de70d40216fe74763598906fd1045a90b952afea
Susan Meyers <smmeyers24@gmail.com>
2018-10-23 17:04:09 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,23 +12,27 @@ class Triangle
   def kind
     if @side1 == @side2 && @side2 == @side3 
       :equilateral
+          raise TriangleError => TriangleError
+            puts 
+          end
       
     elsif @side1 == @side2 || @side1 == @side3 || @side2 == @side3 
       :isosceles
-      
+          raise TriangleError => TriangleError
+        
+            
     else
       :scalene
+         raise TriangleError => TriangleError
+          end
+  end
+        
+    class TriangleError < StandardError
+      def message
+        "The sum of the lenghts of any two sides does not exceed the length of the third side and each side must be larger than 0"
+      end
     end
-        begin
-          raise TriangleError => TriangleError
-            puts error.message
-      
-      class TriangleError < StandardError
-        def message
-          "The sum of the lenghts of any two sides does not exceed the length of the third side and each side must be larger than 0"
-        end
-     end
-   end
+
  end
 
      
