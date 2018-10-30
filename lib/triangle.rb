64e63cd2fa5fd9f173e03243238c1ad5e259923d
Tim Knoll <timothy.knoll@gmail.com>
2018-10-30 16:59:38 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,41 +1,75 @@
+# class Triangle
+#
+#   def initialize (side_1, side_2, side_3)
+#     @tri_sides = []
+#     @tri_sides << side_1
+#     @tri_sides << side_2
+#     @tri_sides << side_3
+#   end
+#
+#   def valid?
+#     sum_1_2 = @tri_sides[0] + @tri_sides[1]
+#     sum_1_3 = @tri_sides[0] + @tri_sides[2]
+#     sum_2_3 = @tri_sides[1] + @tri_sides[2]
+#
+#     if (@tri_sides.none? {|side| side <= 0}) &&
+#       (sum_1_2 > @tri_sides[2] && sum_1_3 > @tri_sides[1] && sum_2_3 > @tri_sides[0])
+#       true
+#     else
+#       false
+#     end
+#   end
+#
+#   def kind
+#     if valid?
+#       if @tri_sides.uniq.length == 1
+#         :equilateral
+#       elsif @tri_sides.uniq.length == 2
+#         :isosceles
+#       else
+#         :scalene
+#       end
+#     else
+#       raise TriangleError
+#     end
+#   end
+# end
+#
+#
+# class TriangleError < StandardError
+#
+# end
+
 class Triangle
 
-  def initialize (side_1, side_2, side_3)
-    @tri_sides = []
-    @tri_sides << side_1
-    @tri_sides << side_2
-    @tri_sides << side_3
-  end
+  attr_reader :a, :b, :c
 
-  def valid?
-    sum_1_2 = @tri_sides[0] + @tri_sides[1]
-    sum_1_3 = @tri_sides[0] + @tri_sides[2]
-    sum_2_3 = @tri_sides[1] + @tri_sides[2]
-
-    if (@tri_sides.none? {|side| side <= 0}) &&
-      (sum_1_2 > @tri_sides[2] && sum_1_3 > @tri_sides[1] && sum_2_3 > @tri_sides[0])
-      true
-    else
-      false
-    end
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
-    if valid?
-      if @tri_sides.uniq.length == 1
-        :equilateral
-      elsif @tri_sides.uniq.length == 2
-        :isosceles
-      else
-        :scalene
-      end
+    valid_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
     else
-      raise TriangleError
+      :scalene
     end
   end
-end
+
+  def valid_triangle
+    real_tri = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each {|side| real_tri << false if side <= 0}
+    raise TriangleError if real_tri.include?(false)
+  end
 
 
-class TriangleError < StandardError
+
+  class TriangleError < StandardError
+  end
 
 end
