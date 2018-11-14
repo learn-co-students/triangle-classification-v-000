598ae8ad89af4c63acb18236f093022f8866da15
Greg Morgan <gregm9999@gmail.com>
2018-11-14 03:11:25 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
+require "pry"
 class Triangle
   # write code here
+  attr_accessor :x, :y, :z
+
+  def initialize(x,y,z)
+    @x=x
+    @y=y
+    @z=z
+  end
+
+  def kind
+    if (x.positive? && y.positive? && z.positive? ) && ((@x+@y>@z && @y+@z>@x && @x+@z>@y))
+      puts z
+      # binding.pry
+      if (@x==@y && @y==@z && @x==@z)
+        return :equilateral
+      elsif (@x==@y && @y!=@z) || (@x!=@y && @y==@z) || (@x==@z && @y!=@z)
+        return :isosceles
+      else
+        return :scalene
+      end
+    else
+        raise TriangleError
+    end
+  end
+
+
+class TriangleError < StandardError
+end
+
 end
