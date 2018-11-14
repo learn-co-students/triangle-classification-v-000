d18332afde1d15d44d30a3c156e7bbf5532b3261
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-11-14 05:40:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,23 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    values = [@a, @b, @c]
+    if values.min <= 0 || values.max >= (values.inject(0, :+) - values.max)
+      raise Triangle::TriangleError
+    elsif values == [@c] * 3
+      return :equilateral
+    elsif values.uniq.size == 2
+      return :isosceles
+    else
+      return :scalene
+    end
+  end
+  class TriangleError < StandardError
+  end
 end
