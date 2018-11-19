7e496410cc6f5e88ea2d7529e1ffbfb49de13685
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-19 16:33:55 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3, :kind
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+  
+  def kind(side1, side2, side3)
+    binding.pry
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    elsif (side1+side2) > side3 || (side1+side3) > side2 || (side2+side3) > side1
+      :scalene
+    else
+        begin
+          raise TriangleError
+          rescue TriangleError => error
+        end
+    end
+  end
+class TriangleError < StandardError
+    puts "Your triangle doesn't make sense."
 end
+
+end 
