0fbc8a8d12abc716bb788d6c69a78d057315beae
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-19 17:10:14 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,19 +9,20 @@ class Triangle
   end
   
   def kind
-   #binding.pry
-    if side1 == side2 && side2 == side3
-      :equilateral
-    elsif side1 == side2 || side1 == side3 || side2 == side3
-      :isosceles
-    elsif (side1+side2) > side3 || (side1+side3) > side2 || (side2+side3) > side1
-      :scalene
-    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
+  # binding.pry
+    if side1 <= 0 || side2 <= 0 || side3 <= 0
         begin
           raise TriangleError
           rescue TriangleError => error
             puts error.message
         end
+    elsif side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 || side1 == side3 || side2 == side3
+      :isosceles
+    elsif (side1+side2) > side3 || (side1+side3) > side2 || (side2+side3) > side1
+      :scalene
+    
     end
   end
 class TriangleError < StandardError
