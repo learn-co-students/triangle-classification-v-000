ce241e986ea9ac55895e2da11ffc3421cfaf4452
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-19 17:00:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,9 +16,7 @@ class Triangle
       :isosceles
     elsif (side1+side2) > side3 || (side1+side3) > side2 || (side2+side3) > side1
       :scalene
-    elsif side1 > 0 && side2 > 0 && side3 > 0
-      :other_triangle
-    else
+    elsif side1 <= 0 || side2 <= 0 || side3 <= 0
         begin
           raise TriangleError
           rescue TriangleError => error
