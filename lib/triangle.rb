c38bba65cd2eef19669a363e2365e1762f92c634
shadowfox3735 <normanpan180@gmail.com>
2018-12-05 22:44:51 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  
+  def initialize(side_1, side_2, side_3)
+    @triangle = []
+    @triangle << side_1
+    @triangle << side_2
+    @triangle << side_3
+  end
+  
+  def valid?
+    if (@triangle.none? {|side| side <= 0}) &&
+    (@triangle[0] + @triangle[1] > @triangle[2] && 
+    @triangle[0] + @triangle[2] > @triangle[1] &&
+    @triangle[1] + @triangle[2] > @triangle[0])
+      true
+    else
+      false
+    end
+  end
+  
+  def kind
+    if valid?
+      if @triangle.uniq.length == 1
+        :equilateral
+      else if @triangle.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+    else
+      raise TriangleError
+    end
+  end
+  
+end
+
+class TriangleError < StandardError
+  
 end
