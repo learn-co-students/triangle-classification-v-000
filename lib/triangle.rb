323367db463b313171b6f1a4811f0c111baa12e9
Katie Moosman <moosman.katie@gmail.com>
2019-01-02 04:14:53 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+   @a = a 
+   @b = b 
+   @c = c
+  end
+  
+  def kind
+    sides_array = [a,b,c]
+    sorted = sides_array.sort
+    min = sorted[0]
+    mid = sorted[1]
+    max = sorted[2]
+    
+    if sides_array.min <= 0 || min + mid <= max
+      raise TriangleError
+    end
+    
+    # if min + mid <= max
+    #   raise TriangleError
+    # end
+  
+    
+    if sides_array.uniq.size <= 1
+      :equilateral
+    elsif sides_array.uniq.length == 2
+      :isosceles
+    else 
+      :scalene
+    end
+    
+  end
+      
+  class TriangleError < StandardError
+    # triangle error code
+  end
+  
 end
