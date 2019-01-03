cc974d254996d541a22b3e95d3c4888d7c9b5dfb
Paula McKinley <pmknyc@optimum.net>
2019-01-03 00:13:32 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,5 +1,6 @@
-class Triangle
+require 'pry'
 
+class Triangle
   # s1, s2, s3 are lengths of the 3 sides
   attr_accessor :s1, :s2, :s3, :sides
 
@@ -9,51 +10,40 @@ class Triangle
     @s3 = s3.to_i
     @sides = [s1, s2, s3]
   end
-  # kind method
+
+# kind method
 #  compute 2 validity tests
-#  if all side lengths > 0 valid test 1 == true
-#    take samples of 2 from sides array, sum them and see if > other side
+#  if all side lengths > 0, validity test 1 = true
+#  then yield to method to test triangle inequality spec
+#  sum of 2 sides > 3rd side; if true, continue with #kind method
 #  output kind of triangle
 
-
   def kind
-    if !(s1 > 0 && s2 > 0 && s3 > 0)
+    #test for nonzero & nonnegative side lengths
+    if s1 <= 0 || s2 <= 0 || s3 <= 0
       begin
         raise TriangleError
         rescue TriangleError => error
         puts error.message1
       end
-    elsif
-      s1 > 0 && s2 > 0 && s3 > 0
-        yield self.tri_inequal(s1,s2,s3)
-    end
 
-    # kind tests
-    if s1 == s2 == s3
-      :equilateral
-    elsif s1 != s2 != s3
-      :scalene
-    else
-      :isoceles
-    end
-  end # kind method
-
-  def tri_inequal(s1, s2, s3)
-    test = []
-    test << (a[0] < a[1] + a[2])
-    test << (a[1] < a[0] + a[2])
-    test << (a[2] < a[1] + a[2])
-    if test.all?(true)
-      self.kind
-    else
+    # test for "triangle inequality" principle
+    elsif s1 >= s2 + s3 || s2 >= s1 + s3 || s3 >= s1 + s2
       begin
         raise TriangleError
         rescue TriangleError => error
         puts error.message2
       end
-    end
-  end #tri_inequal method
 
+    # kind tests
+    elsif s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 != s2 && s2 != s3
+      :scalene
+    else
+      :isoceles
+    end
+  end # kind method
 
   class TriangleError < StandardError
     def message1
