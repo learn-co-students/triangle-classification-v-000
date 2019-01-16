dd5d945f668a66983f7b15d4716b86d630a5e222
Sara Walker <sarawall1187@gmail.com>
2019-01-16 22:21:41 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,15 +9,21 @@ class Triangle
   end
   
   def kind 
-    if @side1 == @side2 && @side2 == @side3 && @side1 == @side3
+    if @side1 <= 0 || @side2 <= 0 || @side3 <= 0 || @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3 && @side1 == @side3
      :equilateral
      elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
      :isosceles
      elsif @side1 != @side2 && @side2 != @side3 && @side1 != @side3
      :scalene
    else 
-     
+    nil 
    end
+  end
+  
+  class TriangleError < RangeError
+  
   end
     
 end
