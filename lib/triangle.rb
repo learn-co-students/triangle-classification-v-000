abd677c6a2875366233b924a071262485b186191
J Fern <jfern20@gmail.com>
2019-02-10 05:21:49 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,31 @@
 class Triangle
+  
+  attr_accessor :side_one, :side_two, :side_three 
    
   def initialize(side_one,side_two,side_three)
     @side_one = side_one
     @side_two = side_two
     @side_three = side_three 
     
+  end
+  
+  
+  def kind 
+    # Triangle rules 
+    #The sum of the lengths of any two sides of a triangle always exceeds the length of the third side. 
+    #This is a principle known as the triangle inequality.
+    if (@side_one + @side_two) <= @side_three || (@side_two + @side_three) <= @side_one || (@side_three + @side_one) <= @side_two 
+    
+    
+    #equilateral 
+    #isosceles
+    #scalene  
   end 
   
   
+class TriangleError < StandardError
+    # triangle error code
+  end
   
   
   
