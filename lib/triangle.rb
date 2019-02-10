5d526ac5e9d78ef39fc8ddb1b9c5bfce2791eacc
J Fern <jfern20@gmail.com>
2019-02-10 03:59:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,16 @@
 class Triangle
-  # write code here
+   
+  def initialize(side_one,side_two,side_three)
+    @side_one = side_one
+    @side_two = side_two
+    @side_three = side_three 
+    
+  end 
+  
+  
+  
+  
+  
+  
+ 
 end
