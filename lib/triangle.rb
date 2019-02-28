5e945ec489a747e21f5eae287659b85a9b4a86c3
witty-collection-3981 <43652427+yisroel96@users.noreply.github.com>
2019-02-28 12:22:55 -0500

    Update triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,8 +19,10 @@ class Triangle
 
   def validate_triangle
     real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    [a, b, c].each do |side|
+      real_triangle << false if side <= 0 
     raise TriangleError if real_triangle.include?(false)
+    end
   end
 
   class TriangleError < StandardError
