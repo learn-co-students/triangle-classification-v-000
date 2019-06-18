d48b518d0f13bc4c34b18ab076e712902af2f8ca
Dan Suarez <suarez.daniel101@gmail.com>
2019-06-18 03:56:00 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
   # write code here
+  attr_accessor :side
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind
+    if side_1 == side_2 && side_2 == side_3
+      :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+      # triangle error code
+    end
+
 end
