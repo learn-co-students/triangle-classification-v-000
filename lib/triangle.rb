9c1e66e9600e7919b99d67ba53673f959750613e
Zain <zain@halai.us>
2019-08-14 15:32:26 -0500

    Update triangle.rb

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,34 +1,31 @@
 class Triangle
-	attr_reader :a, :b, :c
-
+	attr_accessor :a, :b, :c
+	
 	def initialize(a, b, c)
-		@a, @b, @c = a, b, c
+		self.a, self.b, self.c = a, b, c
 	end
 
 	def kind
-		if check_sides || check_triangle_inequality
+		if check_sides? || check_triangle_inequality?
 			raise TriangleError
+		elsif self.a == self.b && self.a == self.c
+			:equilateral
+		elsif self.a == self.b || self.b == self.c || self.a == self.c
+			:isosceles
 		else
-			if a == b && b == c
-				:equilateral
-			elsif a == b || a == c || b == c
-				:isosceles
-			else
-				:scalene
-			end
+			:scalene
 		end
 	end
 
-	def check_sides
-		a <= 0 || b <= 0 || c <= 0
+	def check_sides?
+		self.a <= 0 || self.b <= 0 || self.c <= 0
 	end
 
-	def check_triangle_inequality
-		a + b <= c || a + c <= b || b + c <= a
+	def check_triangle_inequality?
+		self.a + self.b <= self.c || self.a + self.c <= self.b || self.b + self.c <= self.a
 	end
 
 end
-
+	
 class TriangleError < StandardError
-
 end
