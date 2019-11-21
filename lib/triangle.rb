46a5e68500df710306755ea5f5bf8d81c1cc8e90
Angie Goza <angielg9888@gmail.com>
2019-11-21 18:26:12 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+  
+  def kind
+      if @a <= 0 || @b <= 0 || @c <= 0
+          raise TriangleError
+          puts error.message
+      elsif @a + @b <= @c || @b + @c <= @a || @a + @c <= @b
+        raise TriangleError
+        puts error.message
+      elsif  @a == @b && @b == @c && @a == @c
+        :equilateral
+      elsif @a == @b || @b == @c || @a == @c
+        :isosceles
+      elsif @a != @b && @b != @c && @a != @c
+        :scalene
+      end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "This is not a valid triangle!"
+    end
+  end
 end
+
