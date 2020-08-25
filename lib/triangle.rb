1d4d9dd3bb4e7309e4f69dd7fc8c955e5d00a3e0
hkato4188 <hkato@sas.upenn.edu>
2020-08-25 08:45:19 -0400

    created custom error for triangle validation

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,19 @@
+=begin
+Scalene: A scalene triangle is a triangle that has no equal sides.
+Isosceles: An isosceles triangle is a triangle that has two equal sides.
+Equilateral: An equilateral triangle is a triangle that has three equal sides.
+The sum of the lengths of any two sides of a triangle always exceeds the length
+of the third side. This is a principle known as the triangle inequality.
+Further, each side must be larger than 0.
+Triangle inequalitiy theorem:
+a < b + c
+b < a + c
+c < a + b
+=end
 require 'pry'
 
 class Triangle
-  attr_accessor :a, :b, :c
+  attr_reader :a, :b, :c
   def initialize(a, b, c)
     @a = a
     @b = b
@@ -9,26 +21,24 @@ class Triangle
   end
 
   def kind
-    # binding.pry
-    triangle_validation
+    validate_triangle
     if a == b && b == c
       :equilateral
-    elsif (a == b && a != c || a == c && a != b) || a != b && b == c
+    elsif a == b || b == c || a == c
       :isosceles
-    elsif a != b && a != c && b != c
+    else
       :scalene
     end
   end
 
-  def triangle_validation
-    validation_proof_pass = [(a + b > c), (a + c > b), (b + c > a)]
-    [a, b, c].each {|side| validation_proof_pass << false if side <= 0}
-    raise TriangleError if validation_proof_pass.include?(false)
+    def validate_triangle
+      real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+      [a, b, c].each do |side|
+        real_triangle << false if side <= 0
+      raise TriangleError if real_triangle.include?(false)
+      end
     end
 
-  class TriangleError < StandardError
-    def no_size_message
-      puts "triangles cannot have a size length of 0"
+    class TriangleError < StandardError
     end
-  end
 end
