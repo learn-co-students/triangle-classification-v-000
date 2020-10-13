4c9d40eb20be51804b2f3c0ca90dd21cadab61b0
Jaime Johnsen <drivetraindiva@gmail.com>
2020-10-13 23:58:27 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,22 @@
 class Triangle
-  # write code here
+
+  def initialize(side_a, side_b, side_c)
+    @side_a = side_a
+    @side_b = side_b
+    @side_c = side_c
+  end
+
+  def kind
+    if @side_a < 0 || @side_b < 0 || @side_c < 0
+      raise TriangleError
+
+
+  end
+
+  class TriangleError < StandardError
+     def message
+       "You did something wrong"
+     end
+   end
+
 end
